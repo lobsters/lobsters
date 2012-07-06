b128c818c95bce149a0d2d88e4f7b8ec00341bf4
joshua stein <jcs@jcs.org>
2012-07-06 17:33:17 -0500

    add indexes on things that are searched by

---
+++ db/migrate/20120706221602_more_indexes.rb
@@ -0,0 +1,11 @@
+class MoreIndexes < ActiveRecord::Migration
+  def up
+    add_index :stories, [ "is_expired", "is_moderated" ],
+      :name => "is_idxes"
+    add_index :tag_filters, [ "user_id", "tag_id" ],
+      :name => "user_tag_idx"
+  end
+
+  def down
+  end
+end

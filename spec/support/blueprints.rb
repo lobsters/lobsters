6600d36c3bfcc8dbd20ffab23f5d8f0440e2fde9
joshua stein <jcs@jcs.org>
2012-11-23 13:50:09 -0600

    fix tests by creating tag1 and tag2 tags so story blueprint can use them

--- spec/support/blueprints.rb
+++ spec/support/blueprints.rb
@@ -7,14 +7,18 @@ User.blueprint do
   username { "username#{sn}" }
 end
 
+Tag.blueprint do
+  tag { "tag-#{sn}" }
+  description { "tag #{sn}" }
+end
+
+# these need to exist for stories to use them
+Tag.make!(:tag => "tag1")
+Tag.make!(:tag => "tag2")
+
 Story.blueprint do
   user_id { User.make }
   title { "story title #{sn}" }
   url { "http://example.com/#{sn}" }
   tags_a { [ "tag1", "tag2" ] }
-end
-
-Tag.blueprint do
-  tag { "tag-#{sn}" }
-  description { "tag #{sn}" }
 end

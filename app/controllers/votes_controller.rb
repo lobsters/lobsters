ad0691f0bfbb3335c9ae9bf3b2841c12c6b0d4cf
ezy023 <allareri@gmail.com>
2013-03-07 14:54:04 -0500

    change bookmark feature to upvotes only

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,7 +1,7 @@
 class VotesController < ApplicationController
 
 	def stories_user_voted_on
-		@stories_voted_on = @user.voted_stories.all
+		@stories_voted_on = @user.voted_stories.where("vote = '1'")
 	end
 
 end

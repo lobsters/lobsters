0373bcdadb2b0e9b96f6e3a0493963e0a26539db
joshua stein <jcs@jcs.org>
2013-10-18 16:05:00 -0500

    fix moderation log pagination

--- app/controllers/moderations_controller.rb
+++ app/controllers/moderations_controller.rb
@@ -5,8 +5,12 @@ class ModerationsController < ApplicationController
     @pages = Moderation.count
     @page = params[:page] ? params[:page].to_i : 0
 
+    @pages = (Moderation.count / 50).ceil
+
     if @page < 1
       @page = 1
+    elsif @page < @pages
+      @page = @pages
     end
 
     @moderations = Moderation.order("id desc").limit(50).offset((@page - 1) *

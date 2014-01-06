9b5ab9b978bcdde0e14f0b91c0de943b2797d50b
joshua stein <jcs@jcs.org>
2014-01-06 14:49:16 -0600

    try really hard to convert inbound mail to utf8

--- lib/monkey.rb
+++ lib/monkey.rb
@@ -9,3 +9,35 @@ module ActiveRecord
     end
   end
 end
+
+class String
+  def forcibly_convert_to_utf8
+    begin
+      if self.encoding.to_s == "UTF-8" && self.valid_encoding?
+        return self
+      end
+
+      str = self.dup.force_encoding("binary").encode("utf-8",
+        :invalid => :replace, :undef => :replace, :replace => "?")
+
+      if !str.valid_encoding? || str.encoding.to_s != "UTF-8"
+        raise Encoding::UndefinedConversionError
+      end
+
+    rescue Encoding::UndefinedConversionError => e
+      str = self.chars.map{|c|
+        begin
+          c.encode("UTF-8", :invalid => :replace, :undef => :replace)
+        rescue
+          "?".encode("UTF-8")
+        end
+      }.join
+
+      if !str.valid_encoding?
+        raise "still bogus encoding"
+      end
+    end
+
+    str
+  end
+end

2f824a73a3ab1a09b1de0b3af3b79b60721ab5d9
joshua stein <jcs@jcs.org>
2014-01-08 20:41:24 -0600

    shrink story hotness window, things are moving faster now

--- app/models/story.rb
+++ app/models/story.rb
@@ -234,7 +234,7 @@ class Story < ActiveRecord::Base
     end
 
     # TODO: as the site grows, shrink this down to 12 or so.
-    window = 60 * 60 * 48
+    window = 60 * 60 * 36
 
     return -((order * sign) + (self.created_at.to_f / window)).round(7)
   end

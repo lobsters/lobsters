b75468a2c930519dc7459fb3ad99f358dcd20e23
joshua stein <jcs@jcs.org>
2014-02-03 19:05:51 -0600

    move inbound e-mail parsing guts into an extra, add tests for it

---
+++ extras/email_parser.rb
@@ -0,0 +1,110 @@
+class EmailParser
+  attr_reader :sender, :recipient, :email_text, :email
+
+  def initialize(sender, recipient, email_text)
+    @sender = sender
+    @recipient = recipient
+    @email_text = email_text.forcibly_convert_to_utf8
+
+    @email = nil
+    begin
+      # the mail gem stupidly spams STDERR while parsing e-mail, so silence
+      # that stream to avoid anything getting back to postfix
+      Utils.silence_stream(STDERR) do
+        @email = Mail.read_from_string(email_text)
+      end
+    rescue
+    end
+
+    @sending_user = nil
+    @parent = nil
+    @body = nil
+  end
+
+  def user_token
+    @recipient.gsub(/^#{Rails.application.shortname}-/, "").gsub(/@.*/, "")
+  end
+
+  def been_here?
+    !!@email_text.match(/^X-BeenThere: #{Rails.application.shortname}-/i)
+  end
+
+  def sending_user
+    return @sending_user if @sending_user
+
+    if (user = User.where(:mailing_list_enabled => true,
+    :mailing_list_token => user_token).first) && user.is_active?
+      @sending_user = user
+      return user
+    end
+  end
+
+  def parent
+    return @parent if @parent
+
+    irt = self.email[:in_reply_to].to_s.gsub(/[^A-Za-z0-9@\.]/, "")
+
+    if m = irt.match(/^comment\.([^\.]+)\.\d+@/)
+      @parent = Comment.where(:short_id => m[1]).first
+    elsif m = irt.match(/^story\.([^\.]+)\.\d+@/)
+      @parent = Story.where(:short_id => m[1]).first
+    end
+
+    @parent
+  end
+
+  def body
+    return @body if @body
+
+    @possible_charset = nil
+
+    if self.email.multipart?
+      # parts[0] - multipart/alternative
+      #  parts[0].parts[0] - text/plain
+      #  parts[0].parts[1] - text/html
+      if (p = self.email.parts.first.parts.select{|p|
+      p.content_type.match(/text\/plain/i) }).any?
+        @body = p.first.body.to_s
+
+        begin
+          @possible_charset = p.first.content_type_parameters["charset"]
+        rescue
+        end
+
+      # parts[0] - text/plain
+      elsif (p = self.email.parts.select{|p|
+      p.content_type.match(/text\/plain/i) }).any?
+        @body = p.first.body.to_s
+
+        begin
+          @possible_charset = p.first.content_type_parameters["charset"]
+        rescue
+        end
+      end
+
+    # simple one-part
+    elsif self.email.content_type.to_s.match(/text\/plain/)
+      @body = self.email.body.to_s
+
+      begin
+        @possible_charset = self.email.content_type_parameters["charset"]
+      rescue
+      end
+
+    elsif !self.email.content_type.to_s.present?
+      # no content-type header, assume it's text/plain
+      @body = self.email.body.to_s
+    end
+
+    # TODO: use @possible_charset, but did previously forcing the entire
+    # email_text to utf8 screw this up already?
+
+    # try to remove sig lines
+    @body.gsub!(/^-- \n.+\z/m, "")
+
+    # TODO: try to strip out attribution line, followed by an optional blank
+    # line, and then lines prefixed with >
+
+    @body.strip!
+  end
+end
---
+++ spec/models/email_parser_spec.rb
@@ -0,0 +1,61 @@
+require "spec_helper"
+
+describe EmailParser do
+  before(:each) do
+    @user = User.make!
+    @story = Story.make!(:user => @user)
+
+    @commentor = User.make!
+    @comment = Comment.make!(:story => @story, :user => @commentor)
+
+    @emailer = User.make!(:mailing_list_enabled => true)
+
+    @emails = {}
+    Dir.glob("#{Rails.root}/spec/fixtures/inbound_emails/*.eml").
+    each do |f|
+      @emails[File.basename(f).gsub(/\..*/, "")] = File.read(f).
+        gsub(/##SHORTNAME##/, Rails.application.shortname).
+        gsub(/##MAILING_LIST_TOKEN##/, @emailer.mailing_list_token).
+        gsub(/##COMMENT_ID##/, @comment.short_id)
+    end
+  end
+
+  it "can parse a valid e-mail" do
+    parser = EmailParser.new(
+      "user@example.com",
+      Rails.application.shortname +
+      "-#{@emailer.mailing_list_token}@example.org",
+      @emails["1"])
+
+    parser.should_not == nil
+    parser.email.should_not == nil
+
+    parser.user_token.should == @emailer.mailing_list_token
+    parser.been_here?.should == false
+    parser.sending_user.id.should == @emailer.id
+
+    parser.parent.class.should == Comment
+  end
+
+  it "rejects mailing loops" do
+    parser = EmailParser.new(
+      "user@example.com",
+      Rails.application.shortname +
+      "-#{@emailer.mailing_list_token}@example.org",
+      @emails["2"])
+
+    parser.email.should_not == nil
+    parser.been_here?.should == true
+  end
+
+  it "strips signatures" do
+    parser = EmailParser.new(
+      "user@example.com",
+      Rails.application.shortname +
+      "-#{@emailer.mailing_list_token}@example.org",
+      @emails["3"])
+
+    parser.email.should_not == nil
+    parser.body.should == "It hasn't decreased any measurable amount but since the traffic to\nthe site is increasing a bit each week, it's hard to tell."
+  end
+end

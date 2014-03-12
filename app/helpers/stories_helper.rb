af24e4e388b6613661fb870ed0e678e056f0f3ad
joshua stein <jcs@jcs.org>
2014-03-12 17:22:29 -0500

    if a user has had a story moderated in the past 5 days, show guidelines

---
+++ app/helpers/stories_helper.rb
@@ -0,0 +1,19 @@
+module StoriesHelper
+  def show_guidelines?
+    if !@user
+      return true
+    end
+
+    if @user.stories_submitted_count <= 5
+      return true
+    end
+
+    if Moderation.joins(:story).
+    where("stories.user_id = ? AND moderations.created_at > ?", @user.id,
+    5.days.ago).count > 0
+      return true
+    end
+
+    false
+  end
+end

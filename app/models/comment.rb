eec85db1f89933d877ec9bb59c1f8ac72a42db3a
joshua stein <jcs@jcs.org>
2014-04-02 13:14:08 -0500

    bump up max edit times for stories and comments
    not really any harm in letting people correct mistakes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -24,7 +24,7 @@ class Comment < ActiveRecord::Base
   DOWNVOTABLE_DAYS = 7
 
   # after this many minutes old, a comment cannot be edited
-  MAX_EDIT_MINS = 90
+  MAX_EDIT_MINS = (60 * 4)
 
   validate do
     self.comment.to_s.strip == "" &&
--- app/models/story.rb
+++ app/models/story.rb
@@ -13,7 +13,7 @@ class Story < ActiveRecord::Base
   DOWNVOTABLE_DAYS = 14
 
   # after this many minutes old, a story cannot be edited
-  MAX_EDIT_MINS = 30
+  MAX_EDIT_MINS = 90
 
   # days a story is considered recent, for resubmitting
   RECENT_DAYS = 30

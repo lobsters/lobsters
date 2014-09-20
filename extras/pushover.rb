d4c5a84f6a88414ff1a3f148f61d6d16a7293ccc
Łukasz Dubiel <bambucha14@gmail.com>
2014-09-20 02:25:25 +0200

    Remove custom pushover code.

--- extras/pushover.rb
+++ extras/pushover.rb
@@ -1,28 +1,7 @@
 class Pushover
-  cattr_accessor :API_KEY
 
-  # this needs to be overridden in config/initializers/production.rb
-  @@API_KEY = nil
-
-  @@SOUNDS = {}
   def self.sounds
-    if !@@API_KEY
-      return @@SOUNDS
-    end
-
-    if @@SOUNDS.length <= 1
-      begin
-        s = Sponge.new
-        res = s.get("https://api.pushover.net/1/sounds.json?token=#{@@API_KEY}")
-        @@SOUNDS = JSON.parse(res)["sounds"] || {}
-      rescue => e
-        Rails.logger.error "error sending to pushover: #{e.inspect}"
-      end
-
-      @@SOUNDS = { "" => "Device default" }.merge(@@SOUNDS)
-    end
-
-    @@SOUNDS
+    Pushover.sounds
   end
 
   def self.push(user, device, params)
@@ -30,21 +9,8 @@ class Pushover
       return
     end
 
-    begin
-      params[:message] = params[:message].to_s.match(/.{0,512}/m).to_s
+    message_to_send = params[:message][0,511] || "No message"
 
-      if params[:message] == ""
-        params[:message] = "(No message)"
-      end
-
-      s = Sponge.new
-      s.fetch("https://api.pushover.net/1/messages.json", :post, {
-        :token => @@API_KEY,
-        :user => user,
-        :device => device
-      }.merge(params))
-    rescue => e
-      Rails.logger.error "error sending to pushover: #{e.inspect}"
-    end
+    Pushover.notification(message: message_to_send, user: user, device: device)
   end
 end

ddb9215aa04924832108b3c2361ec8ec5accf544
joshua stein <jcs@jcs.org>
2014-11-15 09:52:29 -0600

    log a moderation when hats are granted, since we're open and all that

--- app/models/hat.rb
+++ app/models/hat.rb
@@ -6,6 +6,8 @@ class Hat < ActiveRecord::Base
   validates :user, :presence => true
   validates :granted_by_user, :presence => true
 
+  after_create :log_moderation
+
   def to_html_label
     h = "<span class=\"hat\" title=\"Granted by " <<
       "#{self.granted_by_user.username} on " <<
@@ -25,5 +27,15 @@ class Hat < ActiveRecord::Base
     h << "</span></span>"
 
     h.html_safe
+  end
+
+  def log_moderation
+    m = Moderation.new
+    m.created_at = self.created_at
+    m.user_id = self.user_id
+    m.moderator_user_id = self.granted_by_user_id
+    m.action = "Granted hat \"#{self.hat}\"" + (self.link.present? ?
+      " (#{self.link})" : "")
+    m.save
   end
 end

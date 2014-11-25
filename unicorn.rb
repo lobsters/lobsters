9db404200859a3352d6b70dfe44fc69e3de808a7
Hao Lian <me@haolian.org>
2014-11-24 22:36:19 -0500

    unicorn.rb!

---
+++ unicorn.rb
@@ -0,0 +1,17 @@
+worker_processes 4
+listen 5000
+preload_app true
+
+before_fork do |server, worker|
+  # Disconnect since the database connection will not carry over
+  if defined? ActiveRecord::Base
+    ActiveRecord::Base.connection.disconnect!
+  end
+end
+
+after_fork do |server, worker|
+  # Start up the database connection again in the worker
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.establish_connection
+  end
+end

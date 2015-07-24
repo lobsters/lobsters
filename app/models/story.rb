8282181e4915b9669221cdd716fb7ff3fd97d584
joshua stein <jcs@jcs.org>
2015-07-24 00:34:00 -0500

    Story: shrink hotness window to 24 hours

--- app/models/story.rb
+++ app/models/story.rb
@@ -167,7 +167,7 @@ class Story < ActiveRecord::Base
     end
 
     # TODO: as the site grows, shrink this down to 12 or so.
-    window = 60 * 60 * 36
+    window = 60 * 60 * 24
 
     return -((order * sign) + base +
       ((self.created_at || Time.now).to_f / window)).round(7)

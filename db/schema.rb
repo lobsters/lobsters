2641a9bb409d4c93a159971db7cc08e5b4f59b67
Michael Bernstein <michaelrbernstein@gmail.com>
2017-01-30 11:15:42 -0500

    Change migration value in Schema (#339)

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170119172852) do
+ActiveRecord::Schema.define(version: 20170119192703) do
 
   create_table "comments", force: true do |t|
     t.datetime "created_at",                                                                    null: false

432977c087d52492abb570b94486ee6f1e12d6e3
Carl Chenet <chaica@ohmytux.com>
2017-05-16 19:36:52 +0200

    comments: when showing user threads, always load votes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -274,7 +274,7 @@ class CommentsController < ApplicationController
     comments_by_thread_id = comments.group_by(&:thread_id)
     @threads = comments_by_thread_id.values_at(*thread_ids).compact
 
-    if @user && (@showing_user.id == @user.id)
+    if @user
       @votes = Vote.comment_votes_by_user_for_story_hash(@user.id,
         comments.map(&:story_id).uniq)
 

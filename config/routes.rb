f1ad8922467e6c915fbb23d48369d79ae886bcbf
joshua stein <jcs@jcs.org>
2017-10-10 16:28:05 -0500

    add /messages.json and /messages/sent.json interfaces

--- config/routes.rb
+++ config/routes.rb
@@ -89,7 +89,8 @@ Lobsters::Application.routes.draw do
     get "/comments/page/:page" => "comments#index"
     get "/comments" => "comments#index", :format => /html|rss/
 
-    get "/messages/sent" => "messages#sent"
+    get "/messages/sent" => "messages#sent", :format => /html|json/
+    get "/messages" => "messages#index", :format => /html|json/
     post "/messages/batch_delete" => "messages#batch_delete",
       :as => "batch_delete_messages"
     resources :messages do

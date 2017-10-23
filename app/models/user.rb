3fdaecbb026a9d68c54ce2dd74316e88ad186ecd
Andrew Shu <talklittle@gmail.com>
2017-10-23 11:49:00 -0700

    user: add method is_heavy_self_promoter?

--- app/models/user.rb
+++ app/models/user.rb
@@ -101,6 +101,12 @@ class User < ActiveRecord::Base
   # minimum karma required to process invitation requests
   MIN_KARMA_FOR_INVITATION_REQUESTS = MIN_KARMA_TO_DOWNVOTE
 
+  # proportion of posts authored by user to consider as heavy self promoter
+  HEAVY_SELF_PROMOTER_PROPORTION = 0.51
+
+  # minimum number of submitted stories before checking self promotion
+  MIN_STORIES_CHECK_SELF_PROMOTION = 2
+
   def self.recalculate_all_karmas!
     User.all.each do |u|
       u.karma = u.stories.map(&:score).sum + u.comments.map(&:score).sum
@@ -380,6 +386,20 @@ class User < ActiveRecord::Base
 
   def is_new?
     Time.now - self.created_at <= NEW_USER_DAYS.days
+  end
+
+  def is_heavy_self_promoter?
+    total_count = self.stories_submitted_count
+
+    if total_count < MIN_STORIES_CHECK_SELF_PROMOTION
+      false
+    else
+      authored = Story.where(
+        { :user_id => self.id, :user_is_author => true }
+      ).count
+
+      authored.to_f / total_count >= HEAVY_SELF_PROMOTER_PROPORTION
+    end
   end
 
   def linkified_about

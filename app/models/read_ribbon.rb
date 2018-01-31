c2e8074c0c4f1967c1867bb850957af3656f4755
Peter Bhat Harkins <peter@push.cx>
2018-01-31 13:12:29 -0600

    fix hiding
    Users can hide Stories without visiting their comments pages and creating a
    ReadRibbon. Rather than skip hide_replies_for, this creates a ribbon so that
    they don't get one with is_following defaulted to true should they follow a
    direct link in to the story from somewhere.

--- app/models/read_ribbon.rb
+++ app/models/read_ribbon.rb
@@ -6,7 +6,7 @@ class ReadRibbon < ApplicationRecord
   # StoriesController uses .touch and RepliesController uses update_all
 
   def self.hide_replies_for(story_id, user_id)
-    ribbon = find_by(user_id: user_id, story_id: story_id)
+    ribbon = find_or_create_by(user_id: user_id, story_id: story_id)
     ribbon.is_following = false
     ribbon.save!
   end

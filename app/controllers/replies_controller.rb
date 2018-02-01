0b5567629961df60686e7c807930b796ad2f38dc
Peter Bhat Harkins <peter@push.cx>
2018-02-01 11:58:29 -0600

    fix sql join of user's votes
    An inner join only returns the comments the user has voted on (not useful for
    the Unread page in particular). A left join on comment_id or user_id returns
    duplicate rows (any 'outer apply' is nonstandard). So this just subselects it.

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -47,6 +47,7 @@ class RepliesController < ApplicationController
   # with the current user's vote added by StoriesController.load_user_votes
   def apply_current_vote
     @replies.each do |r|
+      next unless r.current_vote_vote.present?
       r.comment.current_vote = {
         vote: r.current_vote_vote,
         reason: r.current_vote_reason.to_s

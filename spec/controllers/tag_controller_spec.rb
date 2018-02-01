d2fc124807c271d110c830408f568a5a25e5480e
Mason Gup <masongup@gmail.com>
2018-01-31 19:58:09 -0500

    Change test tag name in spec to single word

--- spec/controllers/tag_controller_spec.rb
+++ spec/controllers/tag_controller_spec.rb
@@ -10,8 +10,8 @@ describe TagsController do
 
   context 'create' do
     it 'creates new tags' do
-      post :create, params: { tag: { tag: 'my tag' } }
-      expect(Tag.find_by(tag: 'my tag')).to be_valid
+      post :create, params: { tag: { tag: 'mytag' } }
+      expect(Tag.find_by(tag: 'mytag')).to be_valid
       expect(response).to redirect_to tags_path
     end
 
@@ -23,9 +23,9 @@ describe TagsController do
 
     it 'creates new tags with expected params' do
       post :create, params: { tag: {
-        tag: 'my tag', description: 'desc', is_media: true, hotness_mod: 1.5, privileged: true, inactive: true
+        tag: 'mytag', description: 'desc', is_media: true, hotness_mod: 1.5, privileged: true, inactive: true
       } }
-      tag = Tag.find_by(tag: 'my tag')
+      tag = Tag.find_by(tag: 'mytag')
       expect(tag.description).to eq 'desc'
       expect(tag.is_media).to be true
       expect(tag.hotness_mod).to eq 1.5
@@ -34,7 +34,7 @@ describe TagsController do
     end
 
     it 'creates a moderation with the expected user_id' do
-      post :create, params: { tag: { tag: 'my tag' } }
+      post :create, params: { tag: { tag: 'mytag' } }
       expect(Moderation.order(id: :desc).first.moderator_user_id).to eq user_id
     end
   end
@@ -61,10 +61,10 @@ describe TagsController do
 
     it 'updates with all permitted params' do
       post :update, params: { id: tag.id,  tag: {
-        tag: 'my tag', description: 'desc', hotness_mod: 1.5, privileged: true, inactive: true
+        tag: 'mytag', description: 'desc', hotness_mod: 1.5, privileged: true, inactive: true
       } }
       new_tag = Tag.find(tag.id)
-      expect(new_tag.tag).to eq 'my tag'
+      expect(new_tag.tag).to eq 'mytag'
       expect(new_tag.description).to eq 'desc'
       expect(new_tag.hotness_mod).to eq 1.5
       expect(new_tag.privileged).to be true

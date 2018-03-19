7e0055adcb135b22e591ee35d1699d03ceef686d
Peter Bhat Harkins <peter@push.cx>
2018-03-19 16:44:19 -0500

    rubocop: Style/YodaCondition, 45

--- config/initializers/email.rb
+++ config/initializers/email.rb
@@ -2,7 +2,7 @@ ActionMailer::Base.smtp_settings = {
   :address => ENV.fetch("SMTP_HOST", "127.0.0.1"),
   :port => Integer(ENV.fetch("SMTP_PORT", 25)),
   :domain => Rails.application.domain,
-  :enable_starttls_auto => ("true" == ENV["SMTP_STARTTLS_AUTO"]),
+  :enable_starttls_auto => (ENV["SMTP_STARTTLS_AUTO"] == "true"),
   :user_name => ENV.fetch("SMTP_USERNAME", ""),
   :password => ENV.fetch("SMTP_PASSWORD", ""),
 }

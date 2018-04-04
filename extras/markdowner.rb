cc815596101ac7570ca5c9424389de602c359acf
Peter Bhat Harkins <peter@push.cx>
2018-04-04 09:20:10 -0500

    Fix feed validation errors
    W3C's feed validator service said:
    1. Author should be an email address
    2. Don't use relative links in <description>
    3. Include a link with rel="self"

--- extras/markdowner.rb
+++ extras/markdowner.rb
@@ -52,7 +52,7 @@ class Markdowner
 
       if User.exists?(:username => user[1..-1])
         link = CommonMarker::Node.new(:link)
-        link.url = "/u/#{user[1..-1]}"
+        link.url = Rails.application.root_url + "u/#{user[1..-1]}"
         node.insert_after(link)
 
         link_text = CommonMarker::Node.new(:text)

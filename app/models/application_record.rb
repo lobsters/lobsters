0ea3cedd9fd99e1714f822ef86cbb340146659b3
Peter Bhat Harkins <peter@push.cx>
2018-04-25 11:02:53 -0500

    fix for composing queries

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,6 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  # https://stackoverflow.com/questions/50026344/composing-activerecord-scopes-with-selects
+  scope :select_fix, -> { select(self.arel_table.project(Arel.star)) }
 end

279dfd484d3cfb787e502ac9d0f42c4672825e4a
Félix Baylac-Jacqué <felix@alternativebit.fr>
2018-05-08 18:14:53 +0200

    Document nixos shell environment setup.

--- shell.nix
+++ shell.nix
@@ -1,3 +1,7 @@
+# In order to run lobsters you'll need:
+#   1. To update mysql2 to 0.4.10 in your Gemfile.
+#   2. To use nixpkgs > 89bed5b604cd40f135d2f6a17273a2c4a4374609 
+#        (see <https://github.com/NixOS/nixpkgs/pull/40007>)
 with import <nixpkgs> {}; {
   LobstersEnv = stdenv.mkDerivation {
     name = "Lobsters";

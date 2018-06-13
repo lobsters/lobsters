b41c9e16f4b4d535c362665426c86b36358998cb
Thomas Dziedzic <thomas@pagerduty.com>
2018-06-13 15:02:50 -0500

    expand rails/all

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,24 @@
 require_relative 'boot'
 
-require 'rails/all'
+# included from https://github.com/rails/rails/blob/5-2-stable/railties/lib/rails/all.rb
+require 'rails'
+
+%w(
+  active_record/railtie
+  active_storage/engine
+  action_controller/railtie
+  action_view/railtie
+  action_mailer/railtie
+  active_job/railtie
+  action_cable/engine
+  rails/test_unit/railtie
+  sprockets/railtie
+).each do |railtie|
+  begin
+    require railtie
+  rescue LoadError
+  end
+end
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.

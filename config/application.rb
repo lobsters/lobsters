db152d0d6be1a8b0be72bf1f547d4d0c5db565f3
Thomas Dziedzic <thomas@pagerduty.com>
2018-06-13 15:03:44 -0500

    remove active_storage and action_cable

--- config/application.rb
+++ config/application.rb
@@ -5,12 +5,10 @@ require 'rails'
 
 %w(
   active_record/railtie
-  active_storage/engine
   action_controller/railtie
   action_view/railtie
   action_mailer/railtie
   active_job/railtie
-  action_cable/engine
   rails/test_unit/railtie
   sprockets/railtie
 ).each do |railtie|
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,14 +36,6 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
-  # Store uploaded files on the local file system (see config/storage.yml for options)
-  config.active_storage.service = :local
-
-  # Mount Action Cable outside main process or domain
-  # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
-
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = true
 

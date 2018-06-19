4bd574ddb7a49b6a2b72a11b81af0f9485b3f8d1
Peter Bhat Harkins <peter@push.cx>
2018-06-19 09:14:44 -0500

    rm unused component

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,6 @@ require 'rails'
   action_controller/railtie
   action_view/railtie
   action_mailer/railtie
-  active_job/railtie
   rails/test_unit/railtie
   sprockets/railtie
 ).each do |railtie|

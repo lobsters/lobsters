073a2c1397f8a5505132ce72b4a7498d1ef7a3fa
Thomas Dziedzic <thomasdziedzic0@gmail.com>
2018-08-14 18:05:09 +0000

    finish rails 5.2 migration

--- config/application.rb
+++ config/application.rb
@@ -24,9 +24,11 @@ Bundler.require(*Rails.groups)
 module Lobsters
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.0
+    config.load_defaults 5.2
 
     config.active_record.belongs_to_required_by_default = false
+
+    config.action_view.form_with_generates_remote_forms = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers

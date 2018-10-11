bcb141ff5eef0c04790782844fc81de76aadf500
Jason Schweier <jasons@nulogy.com>
2018-10-11 08:49:41 -0400

    Extract helper method

--- spec/models/story_repository_spec.rb
+++ spec/models/story_repository_spec.rb
@@ -1,37 +1,35 @@
 require "rails_helper"
 
 describe StoryRepository do
-  let(:viewing_user) { create(:user) }
-  let(:repo) { StoryRepository.new(viewing_user) }
+  let(:submitter) { create(:user) }
+  let(:repo) { StoryRepository.new(submitter) }
 
   describe ".newest_by_user" do
     context "when user is viewing their own stories" do
       before do
-        create(:story, user: viewing_user, title: "A story not merged")
-        create(:story, user: viewing_user, title: "A merged story",
+        create(:story, user: submitter, title: "A story not merged")
+        create(:story, user: submitter, title: "A merged story",
                merged_into_story: create(:story, title: "Story merged into"))
-        create(:story, user: viewing_user, title: "A merged story by the same user",
-               merged_into_story: create(:story, user: viewing_user))
+        create(:story, user: submitter, title: "A merged story by the same user",
+               merged_into_story: create(:story, user: submitter))
       end
 
       it "sees their stories" do
-        stories = repo.newest_by_user(viewing_user)
-
-        expect(stories.map(&:title)).to include("A story not merged")
+        expect(story_titles_from(submitter)).to include("A story not merged")
       end
 
       it "sees stories that their story was merged into" do
-        stories = repo.newest_by_user(viewing_user)
-
-        expect(stories.map(&:title)).to_not include("A merged story")
-        expect(stories.map(&:title)).to include("Story merged into")
+        expect(story_titles_from(submitter)).to_not include("A merged story")
+        expect(story_titles_from(submitter)).to include("Story merged into")
       end
 
       it "does not see stories merged into a story they submitted" do
-        stories = repo.newest_by_user(viewing_user)
-
-        expect(stories.map(&:title)).to_not include("A merged story by the same user")
+        expect(story_titles_from(submitter)).to_not include("A merged story by the same user")
       end
     end
+  end
+
+  def story_titles_from(user)
+    repo.newest_by_user(user).map(&:title)
   end
 end

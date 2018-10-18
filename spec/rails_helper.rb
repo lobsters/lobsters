e007bed5990e15070821751f89be2ddb2605aa57
ur5us <juhah@web.de>
2018-10-18 23:12:52 +1300

    Fix failing test data cleanup due to foreign key constraint
    Before this change, running `Tag.destroy_all` would fail due to existing
    `Moderation` records with a foreign key on some of those tags with the
    following message:
    
    ```
    Mysql2::Error:
      Cannot delete or update a parent row: a foreign key constraint fails
      (`lobsters_test`.`moderations`, CONSTRAINT `moderations_tag_id_fk`
      FOREIGN KEY (`tag_id`) REFERENCES `tags` (`id`))
    ```

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,7 @@ require 'rspec/rails'
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f }
 
+Moderation.destroy_all
 Tag.destroy_all
 Tag.create!([{ tag: "tag1" }, { tag: "tag2" }])
 

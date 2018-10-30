1fa0a283535d713d02fe0a61f72a765a80a58f6a
Jason Schweier <jasons@nulogy.com>
2018-10-30 16:44:05 -0400

    Replace ActiveRecord with SQL in data migration

--- db/migrate/20181003234523_initialize_stories_deleted_counts.rb
+++ db/migrate/20181003234523_initialize_stories_deleted_counts.rb
@@ -4,24 +4,17 @@ class InitializeStoriesDeletedCounts < ActiveRecord::Migration[5.2]
       replace low_priority into keystores
       select concat("user:", users.id, ":stories_deleted"), 0
       from users left join stories on users.id = stories.user_id where stories.id is null;
-      
+
       replace low_priority into keystores
       select concat("user:", users.id, ":stories_deleted"),  count(*)
       from users left join stories on users.id = stories.user_id
       where stories.is_expired = true group by users.id;
     SQL
-      User.pluck(:id).each do |user_id|
-        Keystore.put(
-          "user:#{user_id}:stories_deleted",
-          Story.where(user_id: user_id).deleted.count
-        )
-      end
-    end
   end
 
   def down
-    condition = Keystore.arel_table[:key].matches("user:%:stories_deleted")
-
-    Keystore.where(condition).delete_all
+    ActiveRecord::Base.connection.execute <<~SQL
+      delete from keystores where `key` like 'user:%:stories_deleted';
+    SQL
   end
 end

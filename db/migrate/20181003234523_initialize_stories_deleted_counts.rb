cef7bca76927e0c7ad6cbb7e6c4a726e87eaecd9
Peter Bhat Harkins <pushcx@users.noreply.github.com>
2018-10-30 16:14:48 -0400

    Use SQL for data migrations
    Co-Authored-By: jmks <jason.schweier@gmail.com>

--- db/migrate/20181003234523_initialize_stories_deleted_counts.rb
+++ db/migrate/20181003234523_initialize_stories_deleted_counts.rb
@@ -1,6 +1,15 @@
 class InitializeStoriesDeletedCounts < ActiveRecord::Migration[5.2]
   def up
-    Keystore.transaction do
+    ActiveRecord::Base.connection.execute <<~SQL
+      replace low_priority into keystores
+      select concat("user:", users.id, ":stories_deleted"), 0
+      from users left join stories on users.id = stories.user_id where stories.id is null;
+      
+      replace low_priority into keystores
+      select concat("user:", users.id, ":stories_deleted"),  count(*)
+      from users left join stories on users.id = stories.user_id
+      where stories.is_expired = true group by users.id;
+    SQL
       User.pluck(:id).each do |user_id|
         Keystore.put(
           "user:#{user_id}:stories_deleted",

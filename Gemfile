57b43d01f6ed9323831f4567166e16ea6a27b36f
Abdallah Samman <sammanabdallah@gmail.com>
2018-12-06 10:21:59 +0200

    #527 add rubocop cop to disallow form_with and form_for

--- Gemfile
+++ Gemfile
@@ -37,6 +37,8 @@ gem "oauth" # for twitter-posting bot
 gem "mail" # for parsing incoming mail
 gem "sitemap_generator" # for better search engine indexing
 
+gem "ruumba"
+
 group :test, :development do
   gem 'bullet'
   gem 'capybara'
---
+++ extras/prohibit_form_for_and_form_tag.rb
@@ -0,0 +1,23 @@
+module RuboCop
+  module Cop
+    module Style
+      class DisallowFormForandFormTag < Cop
+        MSG = 'Use `.form_with` and remove the `.form_tag` and `.form_for`'.freeze
+
+        def_node_matcher :form_for_exists?, <<-PATTERN
+          (send _ :form_for ...)
+        PATTERN
+
+        def_node_matcher :form_tag_exists?, <<-PATTERN
+          (send _ :form_tag ...)
+        PATTERN
+
+        def on_send(node)
+          return unless form_for_exists?(node) || form_tag_exists?(node)
+
+          add_offense(node)
+        end
+      end
+    end
+  end
+end

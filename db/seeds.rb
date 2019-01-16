0a484e38d1721f331ab5054e384ff63917a32d79
Chris Lloyd <christopher.lloyd@gmail.com>
2019-01-16 06:14:01 -0800

    Give seed admin enough activity to take all normal user actions

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
 pwd = SecureRandom.base58
-User.create(:username => "inactive-user", :email => "inactive-user@example.com", :password => pwd, :password_confirmation => pwd)
+User.create(
+  :username => "inactive-user",
+  :email => "inactive-user@example.com",
+  :password => pwd,
+  :password_confirmation => pwd
+)
 
-User.create(:username => "test", :email => "test@example.com", :password => "test", :password_confirmation => "test", :is_admin => true, :is_moderator => true)
+User.create(
+  :username => "test",
+  :email => "test@example.com",
+  :password => "test",
+  :password_confirmation => "test",
+  :is_admin => true,
+  :is_moderator => true,
+  :karma => [
+    User::MIN_KARMA_TO_SUGGEST,
+    User::MIN_KARMA_TO_DOWNVOTE,
+    User::MIN_KARMA_TO_SUBMIT_STORIES,
+    User::MIN_KARMA_FOR_INVITATION_REQUESTS
+  ].max,
+  :created_at => User::NEW_USER_DAYS.days.ago
+)
 
 Tag.create(:tag => "test")
 

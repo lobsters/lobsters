3a182e24e064151e70b67009612d51e12dcb6285
Brian Kung <brian@callmekung.com>
2019-04-04 17:22:07 -0500

    WIP - what to do about really long words?
    Unless we parse the text and figure out where the glyph boundaries
    are, we might end up splitting a glyph between bytes.

--- spec/scripts/mail_new_activity_spec.rb
+++ spec/scripts/mail_new_activity_spec.rb
@@ -1,7 +1,41 @@
 require_relative '../../script/mail_new_activity'
 
 describe 'EmailSender' do
-  it "encodes text as quoted printable" do
-    expect("Why Use Pointers?".quoted_printable(true)).to eq("=?UTF-8?Q?Why_Use_Pointers=3F?=")
+  context "encoded words" do
+    it "encodes text as quoted printable" do
+      expect("Why  Use Pointers?".quoted_printable(true))
+        .to eq("=?UTF-8?Q?Why?= =?UTF-8?Q?_?= =?UTF-8?Q?_Use?= =?UTF-8?Q?_Pointers=3F?=")
+    end
+
+    # https://tools.ietf.org/html/rfc2047#section-2
+    # An 'encoded-word' may not be more than 75 characters long, including
+    # 'charset', 'encoding', 'encoded-text', and delimiters.  If it is
+    # desirable to encode more text than will fit in an 'encoded-word' of
+    # 75 characters, multiple 'encoded-word's (separated by CRLF SPACE) may
+    # be used.
+    it 'does not allow lines longer than 75 characters' do
+      string = ("foö " * 30).quoted_printable(true)
+      string.lines.each {|line| expect(line.length).to be <= 75 }
+    end
+
+    it 'does not break on multibyte encoded characters' do
+      q_encoded = (1..30).map { 'ö' * 5 }.join(' ').quoted_printable(true)
+
+      without_space, *remainder = q_encoded.split("\n\t")
+
+      expect(without_space).to eq "=?UTF-8?Q?=C3=B6=C3=B6=C3=B6=C3=B6=C3=B6?="
+      expect(remainder).to all eq "=?UTF-8?Q?_=C3=B6=C3=B6=C3=B6=C3=B6=C3=B6?="
+    end
+
+    it 'handles really long words' do
+      # it does not, in fact, handle long words because it would have to
+      # potentially split in the middle of multibyte characters unless we
+      # parsed the glyphs somehow
+      long_string = 'àéîöuū' * 25
+      q_encoded = long_string.quoted_printable(true)
+      expect(q_encoded.length).to be > long_string.length
+
+      q_encoded.lines.each {|line| expect(line.length).to be <= 75 }
+    end
   end
 end

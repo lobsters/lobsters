3fe90020bc471522f758e464273c20462a05c513
Peter Bhat Harkins <peter@push.cx>
2019-05-01 09:06:24 -0500

    Show merged stories to submitters
    Merge #570

--- app/models/story_repository.rb
+++ app/models/story_repository.rb
@@ -19,7 +19,15 @@ class StoryRepository
   end
 
   def newest_by_user(user)
-    Story.base.where(user_id: user.id).order(id: :desc)
+    if @user == user
+      stories = Story.includes(:tags).not_deleted.left_joins(:merged_stories)
+      unmerged = stories.unmerged.where(user_id: user.id)
+      merged_into_others = stories.where(merged_stories_stories: { user_id: user.id })
+
+      unmerged.or(merged_into_others).order(id: :desc)
+    else
+      Story.base.where(user_id: user.id).order("stories.id DESC")
+    end
   end
 
   def newest_including_deleted_by_user(user)

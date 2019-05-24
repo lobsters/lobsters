7f159a8de136c80480324dfcffc5a7a6c4321dd4
Alex Gessner <alex@keyba.se>
2019-05-24 17:01:46 -0400

    add default keybase base_url

--- extras/keybase.rb
+++ extras/keybase.rb
@@ -13,7 +13,7 @@ class Keybase
   def self.avatar_url(kb_username)
     s = Sponge.new
     url = [
-      File.join(@@BASE_URL, '/_/api/1.0/user/pic_url.json?'),
+      File.join(base_url, '/_/api/1.0/user/pic_url.json?'),
       "username=#{kb_username}",
     ].join('')
     res = s.fetch(url, :get).body
@@ -25,7 +25,7 @@ class Keybase
   def self.proof_valid?(kb_username, kb_signature, username)
     s = Sponge.new
     url = [
-      File.join(@@BASE_URL, '/_/api/1.0/sig/proof_valid.json?'),
+      File.join(base_url, '/_/api/1.0/sig/proof_valid.json?'),
       "domain=#{@@DOMAIN}&",
       "kb_username=#{kb_username}&",
       "sig_hash=#{kb_signature}&",
@@ -37,10 +37,14 @@ class Keybase
   end
 
   def self.success_url(kb_username, kb_signature, kb_ua, username)
-    File.join(@@BASE_URL, "/_/proof_creation_success?domain=#{@@DOMAIN}&kb_username=#{kb_username}&username=#{username}&sig_hash=#{kb_signature}&kb_ua=#{kb_ua}")
+    File.join(base_url, "/_/proof_creation_success?domain=#{@@DOMAIN}&kb_username=#{kb_username}&username=#{username}&sig_hash=#{kb_signature}&kb_ua=#{kb_ua}")
   end
 
   def self.default_keybase_avatar_url
     "https://keybase.io/images/icons/icon-keybase-logo-48@2x.png"
+  end
+
+  def self.base_url
+    @@BASE_URL || "https://keybase.io"
   end
 end

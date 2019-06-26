d8c52e2a621435d45a446ca6cac2eb0e7b0c7e57
Thomas Dziedzic <thomasdziedzic0@gmail.com>
2019-06-26 11:01:41 -0500

    fix rubocop issues

--- spec/controllers/csp_controller_spec.rb
+++ spec/controllers/csp_controller_spec.rb
@@ -7,10 +7,17 @@ describe CspController do
         "csp-report" => {
           "blocked-uri" => "data",
           "document-uri" => "http://localhost:3000/s/izi825/hckr_news_hacker_news_sorted_by_time",
-          "original-policy" => "default-src 'none'; img-src *; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; form-action 'self'; report-uri http://localhost:3000/csp-violation-report",
+          "original-policy" => [
+            "default-src 'none';",
+            "img-src *;",
+            "script-src 'self' 'unsafe-inline';",
+            "style-src 'self' 'unsafe-inline';",
+            "form-action 'self';",
+            "report-uri http://localhost:3000/csp-violation-report",
+          ].join(' '),
           "referrer" => "http://localhost:3000/",
-          "violated-directive" => "img-src"
-        }
+          "violated-directive" => "img-src",
+        },
       }
       post :violation_report, body: body.to_json, format: :json
       expect(response).to have_http_status(:ok)

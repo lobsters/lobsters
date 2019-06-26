be6166329b9cdb9ac6a698ddcaeec5db11167b82
Thomas Dziedzic <thomasdziedzic0@gmail.com>
2019-06-26 09:34:30 -0500

    implement content security policy #486

---
+++ spec/controllers/csp_controller_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe CspController do
+  describe '/csp-violation-report' do
+    it 'records the violation' do
+      body = {
+        "csp-report" => {
+          "blocked-uri" => "data",
+          "document-uri" => "http://localhost:3000/s/izi825/hckr_news_hacker_news_sorted_by_time",
+          "original-policy" => "default-src 'none'; img-src *; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; form-action 'self'; report-uri http://localhost:3000/csp-violation-report",
+          "referrer" => "http://localhost:3000/",
+          "violated-directive" => "img-src"
+        }
+      }
+      post :violation_report, body: body.to_json, format: :json
+      expect(response).to have_http_status(:ok)
+    end
+  end
+end

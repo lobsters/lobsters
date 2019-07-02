2d1887cb35f761b7c7fdf95adce74003e19511d0
Thomas Dziedzic <thomasdziedzic0@gmail.com>
2019-07-02 08:55:22 -0500

    set action_view.form_with_generates_remote_forms to default (true) (#687)

--- config/application.rb
+++ config/application.rb
@@ -26,8 +26,6 @@ module Lobsters
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    config.action_view.form_with_generates_remote_forms = false
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading

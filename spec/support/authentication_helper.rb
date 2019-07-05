73074049233b488999ab29695b5e5e134e17903b
Thomas Dziedzic <thomasdziedzic0@gmail.com>
2019-07-05 15:26:32 -0500

    make rubocop happy with authentication_helper

--- spec/support/authentication_helper.rb
+++ spec/support/authentication_helper.rb
@@ -1,27 +1,19 @@
 module AuthenticationHelper
   module ControllerHelper
     def stub_login_as user
-      random_token = ::AuthenticationHelper::common_setup user
+      random_token = "abcdefg".split('').shuffle.join
+      user.update_column(:session_token, random_token)
       session[:u] = random_token
     end
   end
 
   module FeatureHelper
     def stub_login_as user
-      random_token = ::AuthenticationHelper::common_setup user
       # feature specs don't have access to the session store
       visit '/login'
       fill_in 'E-mail or Username:', with: user.email
       fill_in 'Password:', with: user.password
       click_button 'Login'
     end
-  end
-
-  private
-
-  def self.common_setup user
-    random_token = "abcdefg".split('').shuffle.join
-    user.update_column(:session_token, random_token)
-    random_token
   end
 end

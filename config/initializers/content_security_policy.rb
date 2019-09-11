a434da8c2aa0a4fd51ab6a6db6544f0c5e84b463
Peter Bhat Harkins <pushcx@users.noreply.github.com>
2019-09-11 15:30:21 +0000

    Revert "enforce content security policy (#711)"
    This reverts commit 57cf63d3728b15459128ebf04409ef32a4cc7be0.

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -22,4 +22,4 @@ end
 # Report CSP violations to a specified URI
 # For further information see the following documentation:
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy-Report-Only
-Rails.application.config.content_security_policy_report_only = false
+Rails.application.config.content_security_policy_report_only = true

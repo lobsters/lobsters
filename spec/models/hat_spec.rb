8ee9af81d00844856eda80db8d518fe509157d7d
Thomas Dziedzic <thomasdziedzic0@gmail.com>
2019-09-28 22:34:52 -0500

    make rubocop happy

--- spec/models/hat_spec.rb
+++ spec/models/hat_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 describe Hat, type: :model do
   describe '#hat' do
-    it { should validate_presence_of(:hat) }
-    it { should validate_length_of(:hat).is_at_most(255) }
+    it { is_expected.to validate_presence_of(:hat) }
+    it { is_expected.to validate_length_of(:hat).is_at_most(255) }
   end
 
   describe '#link' do
-    it { should validate_length_of(:link).is_at_most(255) }
+    it { is_expected.to validate_length_of(:link).is_at_most(255) }
   end
 end

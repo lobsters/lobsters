fb3f407c1c7d10884d363fc88f3150ac938baea6
Amin Arria <arria.amin@gmail.com>
2019-10-09 11:30:24 -0300

    test specific Invitation and InvitationRequest field validations (#763)

--- spec/factories/invitation.rb
+++ spec/factories/invitation.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :invitation do
+    association(:user)
     sequence(:email) {|n| "user-#{n}@example.com" }
     memo { 'some text for memo' }
   end
--- spec/factories/invitation_request.rb
+++ spec/factories/invitation_request.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :invitation_request do
     name { 'pete smith' }
     sequence(:email) {|n| "user-#{n}@example.com" }
-    memo { 'some text for memo' }
+    memo { 'https://memo' }
     ip_address { '1.1.1.1' }
   end
 end

8893286dd92f8ba5926943566ddf5e7c26cd2e5c
Hunter Madison <hmadison@users.noreply.github.com>
2020-01-20 16:06:26 -0500

    Validate boolean for #745

--- app/models/domain.rb
+++ app/models/domain.rb
@@ -2,4 +2,5 @@ class Domain < ApplicationRecord
   has_many :stories # rubocop:disable Rails/HasManyOrHasOneDependent
 
   validates :domain, presence: true
+  validates :is_tracker, inclusion: { in: [true, false] }
 end

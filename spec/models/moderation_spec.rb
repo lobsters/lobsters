3a1815a19f72135113f98e0d397043fa02a4b8f6
Diep Pham <mrfavadi@gmail.com>
2020-03-03 09:50:26 +0700

    add one_foreign_key_present validator to moderation model (#827)

--- spec/models/moderation_spec.rb
+++ spec/models/moderation_spec.rb
@@ -16,4 +16,31 @@ describe Moderation do
     expect(moderation.errors.messages.dig(:reason))
       .to eq(["is too long (maximum is 16777215 characters)"])
   end
+
+  it "validates a moderation linked to exactly one object" do
+    user = create :user
+    comment = create :comment, user: user
+    domain = create :domain
+    story = create :story, user: user
+    tag = create :tag
+
+    valid_moderations = [Moderation.new(comment: comment),
+                         Moderation.new(domain: domain),
+                         Moderation.new(story: story),
+                         Moderation.new(tag: tag),
+                         Moderation.new(user: user),]
+    expect(valid_moderations).to all(be_valid)
+
+    invalid_moderations = [Moderation.new,
+                           Moderation.new(comment: comment, domain: domain),
+                           Moderation.new(comment: comment, domain: domain, story: story),
+                           Moderation.new(comment: comment, domain: domain, story: story, tag: tag),
+                           Moderation.new(comment: comment, domain: domain, story: story, tag: tag,
+                                          user: user),]
+    invalid_moderations.each do |moderation|
+      expect(moderation).not_to be_valid
+      expect(moderation.errors.messages.dig(:base))
+        .to eq(["moderation should be linked to only one object"])
+    end
+  end
 end

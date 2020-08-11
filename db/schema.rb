17fa1cf8482e25499842af8e6d234e780b6fdd76
Peter Bhat Harkins <peter@push.cx>
2020-08-10 21:24:16 -0500

    categories: unique index on category

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_09_023435) do
+ActiveRecord::Schema.define(version: 2020_08_11_022248) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["category"], name: "index_categories_on_category", unique: true
   end
 
   create_table "comments", id: :bigint, unsigned: true, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|

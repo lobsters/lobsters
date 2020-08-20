c6bbb41e1ea4e75bf5c833322e966fee8f08dd1e
Three Planets Software <threeplanetssoftware@users.noreply.github.com>
2020-08-20 13:58:46 -0400

    Add category_id to moderations table

--- db/schema.rb
+++ db/schema.rb
@@ -2,15 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_11_022248) do
+ActiveRecord::Schema.define(version: 2020_08_20_170829) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "category"
@@ -27,11 +27,11 @@ ActiveRecord::Schema.define(version: 2020_08_11_022248) do
     t.bigint "user_id", null: false, unsigned: true
     t.bigint "parent_comment_id", unsigned: true
     t.bigint "thread_id", unsigned: true
-    t.text "comment", limit: 16777215, null: false
+    t.text "comment", size: :medium, null: false
     t.integer "score", default: 1, null: false
     t.integer "flags", default: 0, null: false, unsigned: true
     t.decimal "confidence", precision: 20, scale: 19, default: "0.0", null: false
-    t.text "markeddown_comment", limit: 16777215
+    t.text "markeddown_comment", size: :medium
     t.boolean "is_deleted", default: false
     t.boolean "is_moderated", default: false
     t.boolean "is_from_email", default: false
@@ -104,7 +104,7 @@ ActiveRecord::Schema.define(version: 2020_08_11_022248) do
     t.string "code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "memo", limit: 16777215
+    t.text "memo", size: :medium
     t.datetime "used_at"
     t.bigint "new_user_id", unsigned: true
     t.index ["new_user_id"], name: "invitations_new_user_id_fk"
@@ -123,7 +123,7 @@ ActiveRecord::Schema.define(version: 2020_08_11_022248) do
     t.bigint "recipient_user_id", null: false, unsigned: true
     t.boolean "has_been_read", default: false
     t.string "subject", limit: 100
-    t.text "body", limit: 16777215
+    t.text "body", size: :medium
     t.string "short_id", limit: 30
     t.boolean "deleted_by_author", default: false
     t.boolean "deleted_by_recipient", default: false
@@ -151,11 +151,12 @@ ActiveRecord::Schema.define(version: 2020_08_11_022248) do
     t.bigint "story_id", unsigned: true
     t.bigint "comment_id", unsigned: true
     t.bigint "user_id", unsigned: true
-    t.text "action", limit: 16777215
-    t.text "reason", limit: 16777215
+    t.text "action", size: :medium
+    t.text "reason", size: :medium
     t.boolean "is_from_suggestions", default: false
     t.bigint "tag_id", unsigned: true
     t.integer "domain_id"
+    t.bigint "category_id"
     t.index ["comment_id"], name: "moderations_comment_id_fk"
     t.index ["created_at"], name: "index_moderations_on_created_at"
     t.index ["domain_id"], name: "index_moderations_on_domain_id"
@@ -189,15 +190,15 @@ ActiveRecord::Schema.define(version: 2020_08_11_022248) do
     t.bigint "user_id", null: false, unsigned: true
     t.string "url", limit: 250, default: ""
     t.string "title", limit: 150, default: "", null: false
-    t.text "description", limit: 16777215
+    t.text "description", size: :medium
     t.string "short_id", limit: 6, default: "", null: false
     t.boolean "is_expired", default: false, null: false
     t.integer "score", default: 1, null: false
     t.integer "flags", default: 0, null: false, unsigned: true
     t.boolean "is_moderated", default: false, null: false
     t.decimal "hotness", precision: 20, scale: 10, default: "0.0", null: false
-    t.text "markeddown_description", limit: 16777215
-    t.text "story_cache", limit: 16777215
+    t.text "markeddown_description", size: :medium
+    t.text "story_cache", size: :medium
     t.integer "comments_count", default: 0, null: false
     t.bigint "merged_story_id", unsigned: true
     t.datetime "unavailable_at"
@@ -274,7 +275,7 @@ ActiveRecord::Schema.define(version: 2020_08_11_022248) do
     t.boolean "is_admin", default: false
     t.string "password_reset_token", limit: 75, collation: "utf8mb4_general_ci"
     t.string "session_token", limit: 75, default: "", null: false, collation: "utf8mb4_general_ci"
-    t.text "about", limit: 16777215, collation: "utf8mb4_general_ci"
+    t.text "about", size: :medium, collation: "utf8mb4_general_ci"
     t.bigint "invited_by_user_id", unsigned: true
     t.boolean "is_moderator", default: false
     t.boolean "pushover_mentions", default: false
@@ -358,6 +359,6 @@ ActiveRecord::Schema.define(version: 2020_08_11_022248) do
   add_foreign_key "votes", "users", name: "votes_user_id_fk"
 
   create_view "replying_comments", sql_definition: <<-SQL
-      select `read_ribbons`.`user_id` AS `user_id`,`comments`.`id` AS `comment_id`,`read_ribbons`.`story_id` AS `story_id`,`comments`.`parent_comment_id` AS `parent_comment_id`,`comments`.`created_at` AS `comment_created_at`,`parent_comments`.`user_id` AS `parent_comment_author_id`,`comments`.`user_id` AS `comment_author_id`,`stories`.`user_id` AS `story_author_id`,`read_ribbons`.`updated_at` < `comments`.`created_at` AS `is_unread`,(select `votes`.`vote` from `votes` where `votes`.`user_id` = `read_ribbons`.`user_id` and `votes`.`comment_id` = `comments`.`id`) AS `current_vote_vote`,(select `votes`.`reason` from `votes` where `votes`.`user_id` = `read_ribbons`.`user_id` and `votes`.`comment_id` = `comments`.`id`) AS `current_vote_reason` from (((`read_ribbons` join `comments` on(`comments`.`story_id` = `read_ribbons`.`story_id`)) join `stories` on(`stories`.`id` = `comments`.`story_id`)) left join `comments` `parent_comments` on(`parent_comments`.`id` = `comments`.`parent_comment_id`)) where `read_ribbons`.`is_following` = 1 and `comments`.`user_id` <> `read_ribbons`.`user_id` and `comments`.`is_deleted` = 0 and `comments`.`is_moderated` = 0 and (`parent_comments`.`user_id` = `read_ribbons`.`user_id` or `parent_comments`.`user_id` is null and `stories`.`user_id` = `read_ribbons`.`user_id`) and `stories`.`score` >= 0 and `comments`.`score` >= 0 and (`parent_comments`.`id` is null or `parent_comments`.`score` >= 0 and `parent_comments`.`is_moderated` = 0 and `parent_comments`.`is_deleted` = 0) and !exists(select 1 from (`votes` `f` join `comments` `c` on(`f`.`comment_id` = `c`.`id`)) where `f`.`vote` < 0 and `f`.`user_id` = `parent_comments`.`user_id` and `c`.`user_id` = `comments`.`user_id` and `f`.`story_id` = `comments`.`story_id` limit 1)
+      select `read_ribbons`.`user_id` AS `user_id`,`comments`.`id` AS `comment_id`,`read_ribbons`.`story_id` AS `story_id`,`comments`.`parent_comment_id` AS `parent_comment_id`,`comments`.`created_at` AS `comment_created_at`,`parent_comments`.`user_id` AS `parent_comment_author_id`,`comments`.`user_id` AS `comment_author_id`,`stories`.`user_id` AS `story_author_id`,(`read_ribbons`.`updated_at` < `comments`.`created_at`) AS `is_unread`,(select `votes`.`vote` from `votes` where ((`votes`.`user_id` = `read_ribbons`.`user_id`) and (`votes`.`comment_id` = `comments`.`id`))) AS `current_vote_vote`,(select `votes`.`reason` from `votes` where ((`votes`.`user_id` = `read_ribbons`.`user_id`) and (`votes`.`comment_id` = `comments`.`id`))) AS `current_vote_reason` from (((`read_ribbons` join `comments` on((`comments`.`story_id` = `read_ribbons`.`story_id`))) join `stories` on((`stories`.`id` = `comments`.`story_id`))) left join `comments` `parent_comments` on((`parent_comments`.`id` = `comments`.`parent_comment_id`))) where ((`read_ribbons`.`is_following` = 1) and (`comments`.`user_id` <> `read_ribbons`.`user_id`) and (`comments`.`is_deleted` = 0) and (`comments`.`is_moderated` = 0) and ((`parent_comments`.`user_id` = `read_ribbons`.`user_id`) or (isnull(`parent_comments`.`user_id`) and (`stories`.`user_id` = `read_ribbons`.`user_id`))) and (`stories`.`score` >= 0) and (`comments`.`score` >= 0) and (isnull(`parent_comments`.`id`) or ((`parent_comments`.`score` >= 0) and (`parent_comments`.`is_moderated` = 0) and (`parent_comments`.`is_deleted` = 0))) and (not(exists(select 1 from (`votes` `f` join `comments` `c` on((`f`.`comment_id` = `c`.`id`))) where ((`f`.`vote` < 0) and (`f`.`user_id` = `parent_comments`.`user_id`) and (`c`.`user_id` = `comments`.`user_id`) and (`f`.`story_id` = `comments`.`story_id`))))))
   SQL
 end

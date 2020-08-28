1e570703adc386b5121bc20faf4cecb470b1d211
Alan Post <adp@prgmr.com>
2020-08-28 12:18:26 -0600

    when there are no votes for a comment, score is 0 not NULL.
    A null score, present when there are no votes for a comment,
    causes an ActiveRecord::NotNullViolation.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -318,7 +318,7 @@ class Comment < ApplicationRecord
     self.flags += flag_delta
     Comment.connection.execute <<~SQL
       UPDATE comments SET
-        score = (select sum(vote) from votes where comment_id = comments.id),
+        score = coalesce((select sum(vote) from votes where comment_id = comments.id), 0),
         flags = (select count(*) from votes where comment_id = comments.id and vote = -1),
         confidence = #{self.calculated_confidence}
       WHERE id = #{self.id.to_i}

1e2cc25ee1559d35f89a850327c5024589dfaa98
Chris Lloyd <christopher.lloyd@gmail.com>
2021-03-27 14:10:29 -0700

    Remove dead code
    This is causing master CI to fail. The code seems useful but it'll live
    on in git history if needed.
    
    Test plan: CI passes

--- app/helpers/traffic_helper.rb
+++ app/helpers/traffic_helper.rb
@@ -8,26 +8,6 @@ module TrafficHelper
 
   def self.traffic_range
     return [0, 2]
-    div = PERIOD_LENGTH * 60
-    start_at = 90.days.ago
-    result = ActiveRecord::Base.connection.execute <<-SQL
-      select
-        min(activity) as low,
-        max(activity) as high
-      from
-        (select
-          -- from_unixtime(s.period * #{div}) as "at",
-          -- s.period,
-          v.n_votes + (c.n_comments * 10) + (s.n_stories * 20) AS activity
-        from
-          (SELECT count(1) AS n_votes,    floor(UNIX_TIMESTAMP(updated_at)/#{div}) AS period FROM votes    WHERE updated_at >= '#{start_at}' GROUP BY period) v,
-          (SELECT count(1) AS n_comments, floor(UNIX_TIMESTAMP(created_at)/#{div}) AS period FROM comments WHERE created_at >= '#{start_at}' GROUP BY period) c,
-          (SELECT count(1) AS n_stories,  floor(UNIX_TIMESTAMP(created_at)/#{div}) AS period FROM stories  WHERE created_at >= '#{start_at}' GROUP BY period) s
-        where
-          s.period = c.period and
-          s.period = v.period) act;
-    SQL
-    result.to_a.first
   end
 
   def self.cached_traffic_range

cc19708575a1432ea9466bc9fe8b58788d910339
Dorian Marié <dorian@dorianmarie.fr>
2021-05-18 19:11:01 +0200

    rake fake_data - generate old enough accounts (#981)

--- app/models/user.rb
+++ app/models/user.rb
@@ -461,7 +461,7 @@ class User < ApplicationRecord
 
   def is_new?
     return true unless self.created_at # unsaved object; in signup flow or a test
-    Time.current - self.created_at <= NEW_USER_DAYS.days
+    self.created_at > NEW_USER_DAYS.days.ago
   end
 
   def add_or_update_keybase_proof(kb_username, kb_signature)

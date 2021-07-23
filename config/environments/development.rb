9c60ac98b2cd786d6281bdd44b53b32eca3ebdb4
Peter Bhat Harkins <peter@push.cx>
2021-07-23 16:32:03 -0500

    fix mail domain in test

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,6 +30,10 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = {
+    host: 'localhost',
+    port: 3000,
+  }
 
   config.action_mailer.perform_caching = false
 

4e2f0e423638e8225516e5d680e632eee5cfcfce
Peter Bhat Harkins <peter@push.cx>
2021-10-26 21:00:08 -0500

    fix loading

--- config/initializers/00_zeitwerk.rb
+++ config/initializers/00_zeitwerk.rb
@@ -1,3 +1,6 @@
+# named 00_zeitwerk because Rails loads these in alphabetical order and
+# production.rb needs these classes loaded
+
 %w{extras lib}.each do |dir|
   Rails.autoloaders.main.push_dir(Rails.root.join(dir))
   Dir[File.join(Rails.root, dir, "*.rb")].sort.each {|l| require l }

c80aa2e1fa87609e14ce551028e62c85815d0850
alternet0 <92338820+alternet0@users.noreply.github.com>
2021-12-30 17:35:30 -0500

    Filter additional tracking query parameters (#1047)
    https://support.google.com/google-ads/answer/9744275?hl=en
    https://support.google.com/optimize/answer/6361119?hl=en#zippy=%2Cexample-parameters-in-redirect-destination-dont-conflict

--- app/models/story.rb
+++ app/models/story.rb
@@ -880,7 +880,7 @@ class Story < ApplicationRecord
     if (match = u.match(/\A([^\?]+)\?(.+)\z/))
       params = match[2].split(/[&\?]/)
       # utm_ is google and many others; sk is medium
-      params.reject! {|p| p.match(/^utm_(source|medium|campaign|term|content)=|^sk=|^fbclid=/) }
+      params.reject! {|p| p.match(/^utm_(source|medium|campaign|term|content|referrer)=|^sk=|^gclid=|^fbclid=/) }
       u = match[1] << (params.any?? "?" << params.join("&") : "")
     end
 

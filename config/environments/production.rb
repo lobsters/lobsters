ef61c52afe260e21fbc9e0423bcc16bd13553e3f
Peter Bhat Harkins <peter@push.cx>
2022-01-22 12:07:51 -0600

    bump uglifier for es6

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,7 +26,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Compress Javascript using a preprocessor.
-  config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = Uglifier.new(harmony: true)
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false

3952d43c6502b176182853201a3d978aad286d0f
Peter Bhat Harkins <peter@push.cx>
2022-01-24 07:02:32 -0600

    Fix RSS encoding (#1052)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -257,8 +257,8 @@ class HomeController < ApplicationController
     @cur_url = domain_path(params[:name])
 
     @rss_link = {
-      title: "RSS 2.0 - For #{@domain}",
-      href: "/domain/#{params[:domain]}.rss",
+      title: "RSS 2.0 - For #{@domain.domain}",
+      href: "/domain/#{@domain.domain}.rss",
     }
 
     respond_to do |format|

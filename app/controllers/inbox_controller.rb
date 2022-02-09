d4f58834ba4e5121bb34f6b14c3ade886f0059f6
Peter Bhat Harkins <peter@push.cx>
2022-02-08 21:45:49 -0600

    extract TODOs to PR body

--- app/controllers/inbox_controller.rb
+++ app/controllers/inbox_controller.rb
@@ -1,7 +1,6 @@
 class InboxController < ApplicationController
   before_action :require_logged_in_user
 
-  # TODO: inbox subnav for messages/the several replies pages
   def index
     if @user.unread_replies_count > 0
       redirect_to replies_unread_path
--- app/models/user.rb
+++ app/models/user.rb
@@ -602,7 +602,6 @@ class User < ApplicationRecord
     @unread_replies_count ||= ReplyingComment.where(user_id: self.id, is_unread: true).count
   end
 
-  # TODO: move ReplyingComment count into Keystore; one query to sum
   def inbox_count
     unread_message_count + unread_replies_count
   end

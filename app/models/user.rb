4c80bb3dc6f6498d8e155a4c9740d04fd4675aa6
Harsh Shandilya <me@msfjarvis.dev>
2023-06-10 04:02:34 +0530

    fix(user): make JSON output use encoded HTML

--- app/models/user.rb
+++ app/models/user.rb
@@ -176,9 +176,11 @@ class User < ApplicationRecord
       attrs.push :karma
     end
 
-    attrs.push :homepage, :about
+    attrs.push :homepage
 
     h = super(:only => attrs)
+
+    h[:about] = self.linkified_about
 
     h[:avatar_url] = self.avatar_url
     h[:invited_by_user] = User.where(id: invited_by_user_id).pluck(:username).first

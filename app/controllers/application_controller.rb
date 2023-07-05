a2831a81ca1e23fb374789b6dd950af76a8429e6
Peter Bhat Harkins <peter@push.cx>
2023-07-05 08:32:24 -0500

    fix for mods in dev mode

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -65,8 +65,8 @@ class ApplicationController < ActionController::Base
   end
 
   def flag_warning
-    return false if Rails.env.development? # expensive because Rails doesn't cache in dev
     @flag_warning_int ||= time_interval('1m')
+    return false if Rails.env.development? # expensive because Rails doesn't cache in dev
     @show_flag_warning ||= (
       @user && !!FlaggedCommenters.new(@flag_warning_int[:param], 1.day).check_list_for(@user)
     )

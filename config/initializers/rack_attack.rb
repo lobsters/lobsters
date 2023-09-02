436408d563735c6119bdf9a88f7332de416726ba
Peter Bhat Harkins <peter@push.cx>
2023-09-02 14:25:47 -0500

    improve note, fix high-level rate-limit

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,16 +1,24 @@
-# if you are looking at this file because your RSS reader is triggering the
-# rate-limiting, you can replace separate checks of /t/c.rss and /t/python.rss
-# with a single check of /t/c,python.rss (you can add many tags)
+# Hi, what brings you here?
+
+# RSS reader with many of our feeds? You can combine separate tag feeds like this:
+# instead of /t/c.rss and /t/python.rss, check /t/c,python.rss
+
+# You're trying to figure out something about site activity? You can just write a database query:
+# https://lobste.rs/about#queries
+
+# You're experimenting with writing a scraper of some sort? I appreciate that you're a fan of the
+# site, but please don't do that to a prod service run by hobbyists. C'mon.
+
+# You're a commercial service? Slow down or email me. pushcx@ our domain.
 
 Rack::Attack.safelist('localhost') do |req|
   '127.0.0.1' == req.ip || '::1' == req.ip
 end
 
-# this will kick in way too early if serving assets via rack
-Rack::Attack.throttle("5 requests per second", limit: 5, period: 1, &:ip)
-
-# we ask scrapers to sleep 1s between hits
-Rack::Attack.throttle("60 requests per minute", limit: 60, period: 60, &:ip)
+# these will kick in way too early if serving assets via rack, so don't
+Rack::Attack.throttle("rate 1", limit: 5, period: 1) { |r| r.ip }
+Rack::Attack.throttle("rate 2", limit: 60, period: 60) { |r| r.ip }
+Rack::Attack.throttle("rate 3", limit: 200, period: 600) { |r| r.ip }
 
 # there's attackers enumeratng usernames, mostly via Tor
 Rack::Attack.throttle("user enumerator", limit: 30, period: 300) do |request|
@@ -26,6 +34,10 @@ end
 
 Rack::Attack.throttle("log4j probe", limit: 1, period: 1.week.to_i) do |request|
   request.ip if request.user_agent.try(:include?, '${')
+end
+
+Rack::Attack.throttle("a particular bad bot", limit: 1, period: 1.week.to_i) do |request|
+  request.ip if request.path.start_with?('//avatars')
 end
 
 # explain the throttle

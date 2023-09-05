14663e0ee551ed0cce427e5765598ce0d32817e3
Peter Bhat Harkins <peter@push.cx>
2023-09-05 07:58:22 -0500

    speed_limit: increase logging for prod bug #1200

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -212,7 +212,7 @@ class Comment < ApplicationRecord
 
     wait = ActionController::Base.helpers
       .distance_of_time_in_words(Time.now, (recent.created_at + delay))
-    Rails.logger.info "breaks_speed_limit: #{user.username} replying to https://lobste.rs/c/#{parent_comment.short_id} flags #{flag_count}"
+    Rails.logger.info "breaks_speed_limit: #{user.username} replying to https://lobste.rs/c/#{parent_comment.short_id} parent_comment_ids (#{parent_comment_ids.join(" ")}) flags #{flag_count} commenter_flag_count #{commenter_flag_count} delay #{delay} delay.ago #{delay.ago} recent #{recent.id}"
     errors.add(
       :comment,
       "Thread speed limit reached, next comment allowed in #{wait}. " +

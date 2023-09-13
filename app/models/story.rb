lobsters/lobsters

commit ab44dcdbf62428a5c3078fc4f4bec96a4872a460

align name of Story and Comment current_vote, convenience methods

app/models/story.rb
@@ -144,10 +144,10 @@ class Story < ApplicationRecord
   # Dingbats, emoji, and other graphics https://www.unicode.org/charts/
   GRAPHICS_RE = /[\u{0000}-\u{001F}\u{2190}\u{2192}-\u{27BF}\u{1F000}-\u{1F9FF}]/
 
-  attr_accessor :editing_from_suggestions, :editor, :fetching_ip,
+  attr_accessor :current_vote, :editing_from_suggestions, :editor, :fetching_ip,
     :is_hidden_by_cur_user, :latest_comment_id,
     :is_saved_by_cur_user, :moderation_reason, :previewing,
-    :seen_previous, :vote
+    :seen_previous
   attr_writer :fetched_response
 
   before_validation :assign_short_id_and_score, on: :create
@@ -222,6 +222,15 @@ class Story < ApplicationRecord
       ModNote.tattle_on_story_domain!(self, "banned")
       errors.add(:url, "is from banned domain #{domain.domain}: #{domain.banned_reason}")
     end
+  end
+
+  # current_vote is the vote loaded for the currently-viewing user
+  def current_flagged?
+    current_vote.try(:[], :vote) == -1
+  end
+
+  def current_upvoted?
+    current_vote.try(:[], :vote) == 1
   end
 
   # all stories with similar urls
@@ -626,9 +635,7 @@ class Story < ApplicationRecord
   end
 
   def show_score_to_user?(u)
-    return true if u&.is_moderator?
-    # cast nil to 0, only show score if user hasn't flagged
-    (!vote || vote[:vote].to_i >= 0)
+    u&.is_moderator? && !current_flagged?
   end
 
   def tagging_changes

app/controllers/comments_controller.rb
@@ -135,11 +135,7 @@ class CommentsController < ApplicationController
       parents = comment.parents.with_thread_attributes.for_presentation
 
       @votes = Vote.comment_votes_by_user_for_comment_ids_hash(@user.id, parents.map(&:id))
-      parents.each do |c|
-        if @votes[c.id]
-          c.current_vote = @votes[c.id]
-        end
-      end
+      parents.each { |c| c.current_vote = @votes[c.id] }
       render "_commentbox", locals: {
         comment: comment,
         parents: parents
@@ -277,11 +273,7 @@ class CommentsController < ApplicationController
     if @user
       @votes = Vote.comment_votes_by_user_for_comment_ids_hash(@user.id, @comments.map(&:id))
 
-      @comments.each do |c|
-        if @votes[c.id]
-          c.current_vote = @votes[c.id]
-        end
-      end
+      @comments.each { |c| c.current_vote = @votes[c.id] }
     end
 
     respond_to do |format|
@@ -360,11 +352,7 @@ class CommentsController < ApplicationController
       @user.clear_unread_replies!
       @votes = Vote.comment_votes_by_user_for_story_hash(@user.id, @threads.map(&:story_id).uniq)
 
-      @threads.each do |c|
-        if @votes[c.id]
-          c.current_vote = @votes[c.id]
-        end
-      end
+      @threads.each { |c| c.current_vote = @votes[c.id] }
     end
   end
 

app/controllers/search_controller.rb
@@ -7,19 +7,13 @@ class SearchController < ApplicationController
     @search = Search.new(search_params, @user)
 
     if @user && @search.results
-      # TODO rename Story.vote to match Comment.current_vote
       if params[:what] == "stories"
         votes = Vote.story_votes_by_user_for_story_ids_hash(@user.id, @search.results.map(&:id))
-        @search.results.each do |r|
-          r.vote = votes.try(:[], r.id)
-        end
       end
       if params[:what] == "comments"
         votes = Vote.comment_votes_by_user_for_comment_ids_hash(@user.id, @search.results.map(&:id))
-        @search.results.each do |r|
-          r.current_vote = votes.try(:[], r.id)
-        end
       end
+      @search.results.each { |r| r.current_vote = votes.try(:[], r.id) }
     end
   end
 

app/controllers/stories_controller.rb
@@ -108,7 +108,7 @@ class StoriesController < ApplicationController
     @story.user_id = @user.id
     @story.previewing = true
 
-    @story.vote = Vote.new(vote: 1)
+    @story.current_vote = Vote.new(vote: 1)
     @story.score = 1
 
     @story.valid?
@@ -424,7 +424,7 @@ class StoriesController < ApplicationController
     story ||= Story.find(params[:id]) if @user&.is_admin?
 
     if @user && story
-      story.vote = Vote.find_by(
+      story.current_vote = Vote.find_by(
         user: @user,
         story: story.id,
         comment: nil
@@ -458,9 +458,7 @@ class StoriesController < ApplicationController
 
   def load_user_votes
     if @user
-      if (v = Vote.where(user_id: @user.id, story_id: @story.id, comment_id: nil).first)
-        @story.vote = {vote: v.vote, reason: v.reason}
-      end
+      @story.current_vote = Vote.find_by(user: @user, story: @story, comment: nil)
 
       @story.is_hidden_by_cur_user = @story.is_hidden_by_user?(@user)
       @story.is_saved_by_cur_user = @story.is_saved_by_user?(@user)
@@ -468,11 +466,7 @@ class StoriesController < ApplicationController
       @votes = Vote.comment_votes_by_user_for_story_hash(
         @user.id, @story.merged_stories.ids.push(@story.id)
       )
-      @comments.each do |c|
-        if @votes[c.id]
-          c.current_vote = @votes[c.id]
-        end
-      end
+      @comments.each { |c| c.current_vote = @votes[c.id] }
     end
   end
 

app/models/comment.rb
@@ -224,6 +224,15 @@ class Comment < ApplicationRecord
     self.markeddown_comment = generated_markeddown_comment
   end
 
+  # current_vote is the vote loaded for the currently-viewing user
+  def current_flagged?
+    current_vote.try(:[], :vote) == -1
+  end
+
+  def current_upvoted?
+    current_vote.try(:[], :vote) == 1
+  end
+
   def delete_for_user(user, reason = nil)
     Comment.record_timestamps = false
 
@@ -523,7 +532,7 @@ class Comment < ApplicationRecord
     (
       (created_at && created_at < 36.hours.ago) ||
       !SCORE_RANGE_TO_HIDE.include?(self.score)
-    ) && (!current_vote || current_vote[:vote] >= 0)
+    ) && !current_flagged?
   end
 
   def to_param

app/models/stories_paginator.rb
@@ -34,9 +34,7 @@ class StoriesPaginator
       ss = SavedStory.where(user_id: @user.id, story_id: scope.map(&:id)).map(&:story_id)
 
       scope.each do |s|
-        if votes[s.id]
-          s.vote = votes[s.id]
-        end
+        s.current_vote = votes[s.id]
         if hs.include?(s.id)
           s.is_hidden_by_cur_user = true
         end

app/models/user.rb
@@ -285,7 +285,7 @@ class User < ApplicationRecord
     elsif obj.is_a?(Story)
       if obj.is_flaggable?
         return true
-      elsif obj.vote == -1
+      elsif obj.current_flagged?
         # user can unvote
         return true
       end

app/views/comments/_comment.html.erb
@@ -1,15 +1,16 @@
-<% flagged = comment.current_vote && comment.current_vote[:vote] == -1 %>
 <input id="comment_folder_<%= comment.short_id %>"
   class="comment_folder_button" type="checkbox"
   <%= defined?(force_open) ||
       @user.try(:is_moderator?) ||
-      (comment.score > Comment::COLLAPSE_SCORE && !flagged) ? "" : "checked" %>>
+      (comment.score > Comment::COLLAPSE_SCORE && !comment.current_flagged?) ? "" : "checked" %>>
 
 <div id="c_<%= comment.short_id %>"
-data-shortid="<%= comment.short_id if comment.persisted? %>"
-class="comment <%= comment.current_vote ? (comment.current_vote[:vote] == 1 ?
-"upvoted" : "flagged") : "" %>
-  <%= comment.score < Comment::SCORE_RANGE_TO_HIDE.first ? "bad" : "" %>">
+  data-shortid="<%= comment.short_id if comment.persisted? %>"
+  class="comment
+    <%= comment.current_upvoted? ? "upvoted" : "" %>
+    <%= comment.current_flagged? ? "flagged" : "" %>
+    <%= comment.score < Comment::SCORE_RANGE_TO_HIDE.first ? "bad" : "" %>
+  ">
 
   <% if defined?(show_tree_lines) && show_tree_lines %>
     <label for="comment_folder_<%= comment.short_id %>"
@@ -97,11 +98,11 @@ class="comment <%= comment.current_vote ? (comment.current_vote[:vote] == 1 ?
           <a tabindex="0" class="comment-disowner" href="#">disown</a>
         <% end %>
 
-        <% if can_flag && !flagged %>
+        <% if can_flag && !comment.current_flagged? %>
           <span class="dropdown_parent">
             | <a class="flagger">flag</a>
           </span>
-        <% elsif flagged %>
+        <% elsif comment.current_flagged? %>
           | <a class="flagger">unflag</a>
         <% else %>
           <span class="flagger flagger_stub"></span>
@@ -124,7 +125,7 @@ class="comment <%= comment.current_vote ? (comment.current_vote[:vote] == 1 ?
             <% if (s = comment.vote_summary_for_user(@user).downcase).present? %>
               | <%= s %>
             <% end %>
-          <% elsif comment.current_vote && comment.current_vote[:vote] == -1 %>
+          <% elsif comment.current_flagged? %>
             | -1
             <%= Vote::ALL_COMMENT_REASONS[comment.current_vote[:reason]].downcase %>
           <% end %>

commit a04e70a0693adc5b1f2c82dafd889a0373f37203

fix display of story scores, broken in ab44dcdb

app/models/story.rb
@@ -194,7 +194,7 @@ class Story < ApplicationRecord
     if most_recent_similar&.is_recent?
       errors.add(:url, "has already been submitted within the past #{RECENT_DAYS} days")
       true
-    elsif most_recent_similar && user && user.is_new?
+    elsif most_recent_similar && user&.is_new?
       errors.add(:url, "cannot be resubmitted by new users")
       true
     else
@@ -635,7 +635,7 @@ class Story < ApplicationRecord
   end
 
   def show_score_to_user?(u)
-    u&.is_moderator? && !current_flagged?
+    u&.is_moderator? || !current_flagged?
   end
 
   def tagging_changes
cdde144b0ddf741c22405510822b382e0c5ce1f7
Peter Bhat Harkins <peter@push.cx>
2023-12-17 21:09:51 -0600

    spec update for previous

--- spec/factories/invitation_request.rb
+++ spec/factories/invitation_request.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
   factory :invitation_request do
     name { "pete smith" }
     sequence(:email) { |n| "user-#{n}@example.com" }
-    memo { "https://memo" }
+    memo { "https://memo.example" }
     ip_address { "1.1.1.1" }
   end
 end

9de6363ef66ddae6eeff998cfa3369d27efaf6bf
Jannes Stubbemann <jannes.stubbemann@gmail.com>
2023-12-29 19:49:30 +0100

    [email] adapt to https://guides.rubyonrails.org/action_mailer_basics.html\#action-mailer-configuration-for-gmail

--- config/initializers/email.rb
+++ config/initializers/email.rb
@@ -1,10 +1,14 @@
 # typed: false
 
+ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
   address: ENV.fetch("SMTP_HOST", "127.0.0.1"),
   port: Integer(ENV.fetch("SMTP_PORT", 25)),
   domain: Rails.application.domain,
-  enable_starttls_auto: (ENV["SMTP_STARTTLS_AUTO"] == "true"),
   user_name: ENV.fetch("SMTP_USERNAME", ""),
-  password: ENV.fetch("SMTP_PASSWORD", "")
+  password: ENV.fetch("SMTP_PASSWORD", ""),
+  authentication:  'plain',
+  enable_starttls: true,
+  open_timeout:    5,
+  read_timeout:    5
 }

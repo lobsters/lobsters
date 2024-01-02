a4b9d77970d9f83bf5484c3bcb200c3716cc0400
Jannes Stubbemann <jannes.stubbemann@gmail.com>
2024-01-02 14:57:28 +0100

    [tags] use minimal ones from lobste.rs

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,19 +23,10 @@ User.create(
 )
 
 # Define categories and their corresponding tags
+# https://lobste.rs/tags
 categories_with_tags = {
-  "Quantum_Computing" => ["Superconducting_Qubits", "Trapped_Ions", "Neutral_Atoms", "Quantum_Dots", "Topological_Qubits", "Photonics", "Quantum_Algorithms", "Error_Correction", "Quantum_Software", "Quantum_Supremacy"],
-  "Quantum_Physics_Theory" => ["Quantum_Entanglement", "Quantum_Field_Theory", "Nonlocality", "Quantum_Gravity", "Bell_Tests"],
-  "Quantum_Materials" => ["Nanofabrication", "Quantum_Dots", "Photonic_Chips", "Topological_Insulators", "Quantum_Metamaterials"],
-  "Quantum_Applications" => ["Quantum_Sensing", "Quantum_Metrology", "Quantum_Imaging", "Quantum_Communications", "Quantum_Simulation"],
-  "Quantum_Industry" => ["Quantum_Startups", "Venture_Capital", "Industry_Partnerships", "Patent_Landscape", "Market_Analysis"],
-  "Quantum_Education" => ["MOOCs", "University_Programs", "Public_Lectures", "Educational_Initiatives", "Outreach_Programs"],
-  "Quantum_Policy_Ethics" => ["Data_Security", "Quantum_Ready_Regulations", "Ethical_AI", "IP_Rights", "Privacy_Issues"],
-  "Quantum_Research_Collab" => ["International_Collab", "Research_Grants", "Laboratory_Techniques", "Paper_Discussions", "Experimental_Setups"],
-  "Quantum_Information" => ["Quantum_Cryptography", "QKD_Protocols", "Information_Theory", "Quantum_Coding", "Entropy_and_Information"],
-  "Quantum_Tech_Future" => ["Quantum_Roadmaps", "Future_Predictions", "Theoretical_Breakthroughs", "Interdisciplinary", "Quantum_AI"],
-  "Quantum_Networking" => ["Quantum_Repeaters", "Network_Protocols", "Satellite_QKD", "Teleportation_Experiments", "Quantum_Internet"],
-  "Events_Confs_Workshops" => ["Upcoming_Conferences", "Workshop_Highlights", "Networking_Events", "Virtual_Seminars", "Community_Meetups"]
+  "format" => ["ask", "audio", "book", "pdf", "show", "slides", "transcript", "video"],
+  "quantumnews" => ["announce", "interview", "meta"],
 }
 
 # Iterate over the categories and their tags

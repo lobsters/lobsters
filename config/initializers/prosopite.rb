cb5a66f11c6461fb095998a922ac578c0e05d4f3
Mikalay <32986916+mikalai-yankouski@users.noreply.github.com>
2024-02-29 17:20:36 +0300

    change log path
    Co-authored-by: Peter Bhat Harkins <pushcx@users.noreply.github.com>

--- config/initializers/prosopite.rb
+++ config/initializers/prosopite.rb
@@ -1,5 +1,5 @@
 if Rails.env.production?
-  Prosopite.custom_logger = Logger.new("log/n_plus_one_detection.log")
+  Prosopite.custom_logger = Logger.new("/srv/lobste.rs/log/n_plus_one_detection.log")
 else
   Prosopite.raise = true
 end

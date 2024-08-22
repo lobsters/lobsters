8d5b912a7efc27fa20cc66816211b1cf0e339971
Peter Bhat Harkins <peter@push.cx>
2024-08-22 10:55:24 -0500

    add specs and redirects from .../latest

--- app/controllers/story_urls_controller.rb
+++ app/controllers/story_urls_controller.rb
@@ -3,19 +3,24 @@
 class StoryUrlsController < ApplicationController
   def all
     url = params.require(:url)
-    raise ActionController::ParameterMissing.new("No URL") if url.blank?
+
     respond_to do |format|
       format.json {
-        render json: Story.find_similar_by_url(url).map(&:as_json)
+        render json: Story.find_similar_by_url(url).for_presentation
       }
     end
   end
 
   def latest
     url = params.require(:url)
-    raise ActionController::ParameterMissing.new("No URL") if url.blank?
 
     similar_stories = Story.find_similar_by_url(url)
-    redirect_to similar_stories[0].comments_path unless similar_stories.empty?
+    if similar_stories.any?
+      redirect_to similar_stories.first.comments_path
+    elsif @user
+      redirect_to new_story_path, url: url
+    else
+      raise ActiveRecord::RecordNotFound
+    end
   end
 end

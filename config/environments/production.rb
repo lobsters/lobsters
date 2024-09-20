ce3b65a447e01e8cd78fc45c6671274f485aed4b
Gabriel Simmer <g@gmem.ca>
2024-09-20 16:06:13 +0100

    Production active_storage for /var/lobste.rs/storage

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,6 +109,8 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  config.active_storage.service = :production
 end
 
 # disable some excessive logging in production

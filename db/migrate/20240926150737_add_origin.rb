ee6b3c996c5c20cf1d9fc335eb4d476ca9da98db
Peter Bhat Harkins <peter@push.cx>
2024-10-10 14:02:51 -0500

    fix migration - can't write stories_count before it exists

--- db/migrate/20240926150737_add_origin.rb
+++ db/migrate/20240926150737_add_origin.rb
@@ -1,5 +1,11 @@
 class AddOrigin < ActiveRecord::Migration[7.2]
   def change
+    change_table :domains, bulk: true do
+      add_column :domains, :selector, :string, null: true
+      add_column :domains, :replacement, :string, null: true
+      add_column :domains, :stories_count, :integer, null: false, default: 0
+    end
+
     # bad data in prod lobsters; using &. so this runs clean on sister sites
     # unused extra domain
     Domain.find_by(id: 20017, domain: "codingunicorn.dev")&.destroy!
@@ -9,10 +15,7 @@ class AddOrigin < ActiveRecord::Migration[7.2]
     Domain.find_by(id: 20553, domain: "aaronhawley.livejournal.com")&.destroy!
 
     change_table :domains, bulk: true do
-      add_column :domains, :selector, :string, null: true
-      add_column :domains, :replacement, :string, null: true
       add_index :domains, :domain, unique: true
-      add_column :domains, :stories_count, :integer, null: false, default: 0
     end
 
     create_table :origins do |t|

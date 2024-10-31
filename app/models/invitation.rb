lobsters/lobsters

commit 43a93ca24e28214922b3eaa4aaa25cc4133782d8

lengths: accomodate outlier records

app/models/invitation.rb
@@ -13,7 +13,7 @@ class Invitation < ApplicationRecord
     end
   end
 
-  validates :code, :email, :memo, length: {maximum: 255}
+  validates :code, :email, :memo, length: {maximum: 375}
 
   before_validation :create_code, on: :create
 

app/models/message.rb
@@ -17,7 +17,8 @@ class Message < ApplicationRecord
   attr_reader :recipient_username
 
   validates :subject, length: {in: 1..100}
-  validates :body, length: {maximum: 65_535}
+  validates :body, length: {maximum: 70_000}, on: :update # for weird old data
+  validates :body, length: {maximum: 8_192}, on: :create # for weird old data
   validates :short_id, length: {maximum: 30}
   validates :has_been_read, :deleted_by_author, :deleted_by_recipient, inclusion: {in: [true, false]}
   validate :hat do

db/migrate/20241017011246_active_record_doctor_incorrect_length_validation.rb
@@ -2,12 +2,12 @@ class ActiveRecordDoctorIncorrectLengthValidation < ActiveRecord::Migration[7.2]
   def up
     change_column :links, :url, :string, limit: 250
     change_column :invitation_requests, :memo, :text, limit: 255
-    change_column :invitations, :memo, :text, limit: 255
+    change_column :invitations, :memo, :text, limit: 375
     change_column :hat_requests, :comment, :text, limit: 65_535
     change_column :hats, :short_id, :string, limit: 10
     change_column :categories, :category, :string, limit: 25
     change_column :stories, :description, :text, limit: 65_535
-    change_column :messages, :body, :text, limit: 65_535
+    change_column :messages, :body, :text, limit: 70_000
   end
 
   def down

db/schema.rb
@@ -138,7 +138,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_17_011246) do
     t.string "code"
     t.datetime "created_at", precision: nil, null: false
     t.datetime "updated_at", precision: nil, null: false
-    t.text "memo", size: :tiny
+    t.text "memo"
     t.datetime "used_at", precision: nil
     t.bigint "new_user_id", unsigned: true
     t.index ["new_user_id"], name: "invitations_new_user_id_fk"
@@ -181,7 +181,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_17_011246) do
     t.bigint "recipient_user_id", null: false, unsigned: true
     t.boolean "has_been_read", default: false, null: false
     t.string "subject", limit: 100
-    t.text "body"
+    t.text "body", size: :medium
     t.string "short_id", limit: 30
     t.boolean "deleted_by_author", default: false, null: false
     t.boolean "deleted_by_recipient", default: false, null: false

commit a41f3b383c7075d1fc371d1c76085d68f10f590d

fix specs broken by 43a93ca2

app/models/invitation.rb
@@ -13,7 +13,8 @@ class Invitation < ApplicationRecord
     end
   end
 
-  validates :code, :email, :memo, length: {maximum: 375}
+  validates :code, :email, length: {maximum: 255}
+  validates :memo, length: {maximum: 375}
 
   before_validation :create_code, on: :create
 

spec/models/invitation_spec.rb
@@ -22,8 +22,8 @@ describe Invitation do
   end
 
   it "has a limit on the memo field" do
-    invitation = build(:invitation, memo: "a" * 256)
+    invitation = build(:invitation, memo: "a" * 376)
     invitation.valid?
-    expect(invitation.errors[:memo]).to eq(["is too long (maximum is 255 characters)"])
+    expect(invitation.errors[:memo]).to eq(["is too long (maximum is 375 characters)"])
   end
 end
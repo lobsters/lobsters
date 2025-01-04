39fde15f918b23a06f4d0343504312e17d002075
Rahul Gupta <rahul1990gupta@gmail.com>
2025-01-04 14:53:12 +0530

    adds credentials and authentication

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,7 @@
 # rubocop:disable Rails/ApplicationController
 class AdminController < ActionController::Base
+  http_basic_authenticate_with \
+    name: Rails.application.credentials.mission_control.user, 
+    password: Rails.application.credentials.mission_control.password
 end
 # rubocop:enable Rails/ApplicationController

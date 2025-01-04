78995e798143a27a7c96d68fed2a8928e44f2706
Rahul Gupta <rahul1990gupta@gmail.com>
2025-01-04 15:02:28 +0530

    adds default value

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,7 +1,7 @@
 # rubocop:disable Rails/ApplicationController
 class AdminController < ActionController::Base
   http_basic_authenticate_with \
-    name: Rails.application.credentials.dig(:mission_control, :user),
-    password: Rails.application.credentials.dig(:mission_control, :password)
+    name: Rails.application.credentials.dig(:mission_control, :user) || "test_user",
+    password: Rails.application.credentials.dig(:mission_control, :password) || "test_password"
 end
 # rubocop:enable Rails/ApplicationController

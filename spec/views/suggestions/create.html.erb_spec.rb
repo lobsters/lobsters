1feecadfe25f9d879452d4a1e7d78bb1462aec53
Rahul Gupta <rahul1990gupta@gmail.com>
2025-01-04 16:17:50 +0530

    cleanup

--- spec/views/suggestions/create.html.erb_spec.rb
+++
@@ -1,5 +0,0 @@
-require "rails_helper"
-
-RSpec.describe "suggestions/create.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
--- spec/views/suggestions/new.html.erb_spec.rb
+++
@@ -1,5 +0,0 @@
-require "rails_helper"
-
-RSpec.describe "suggestions/new.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
-end

f65f14eb9351e2200cb822625dac8264a7b51452
Peter Bhat Harkins <peter@push.cx>
2025-01-07 00:58:58 -0600

    Fix the build and other devs from 259b9117

--- extras/maxmind.rb
+++ extras/maxmind.rb
@@ -1,7 +1,11 @@
 module Maxmind
+  DB_PATH = "#{ENV["HOME"]}/dbip.mmdb".freeze
+
   def self.uk? ip
+    return false unless File.exist? DB_PATH
+
     # database from https://db-ip.com/db/download/ip-to-country-lite
-    @db ||= MaxMindDB.new("#{ENV["HOME"]}/dbip.mmdb")
+    @db ||= MaxMindDB.new(DB_PATH)
     result = @db.lookup(ip)
     return false unless result.found?
     result.country.iso_code == "GB"

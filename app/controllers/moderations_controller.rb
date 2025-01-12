c5cbb0299e7ece6a3b7dcd75f820646c068a6b46
Caius Durling <dev@caius.name>
2025-01-12 09:57:09 +0000

    Only use params we expect in moderation index
    These are used to generate the pagination links, which will break if `:controller` or `:action` are passed as parameters due to Rails internals. We shouldn't be "forwarding" random parameters from the user, but generating a link only with the params we expect to be in the URL.
    
    This doesn't currently appear to be a security issue, but it would've been a useful place for injecting things into links on the page if required to pair with another attack later.

--- app/controllers/moderations_controller.rb
+++ app/controllers/moderations_controller.rb
@@ -9,15 +9,15 @@ class ModerationsController < ApplicationController
     @title = "Moderation Log"
     @moderators = ["(All)", "(Users)"] + User.moderators.map(&:username)
 
-    @moderator = params.fetch("moderator", "(All)")
+    @moderator = moderation_params.fetch("moderator", "(All)")
     @what = {
-      stories: params.dig(:what, :stories),
-      comments: params.dig(:what, :comments),
-      tags: params.dig(:what, :tags),
-      users: params.dig(:what, :users),
-      domains: params.dig(:what, :domains),
-      origins: params.dig(:what, :origins),
-      categories: params.dig(:what, :categories)
+      stories: moderation_params.dig(:what, :stories),
+      comments: moderation_params.dig(:what, :comments),
+      tags: moderation_params.dig(:what, :tags),
+      users: moderation_params.dig(:what, :users),
+      domains: moderation_params.dig(:what, :domains),
+      origins: moderation_params.dig(:what, :origins),
+      categories: moderation_params.dig(:what, :categories)
     }
     @what.transform_values! { true } if @what.values.none?
 
@@ -48,7 +48,7 @@ class ModerationsController < ApplicationController
 
     # paginate
     @pages = (@moderations.count / ENTRIES_PER_PAGE).ceil
-    @page = params[:page].to_i
+    @page = moderation_params[:page].to_i
     if @page == 0
       @page = 1
     elsif @page < 0 || @page > (2**32) || @page > @pages
@@ -59,5 +59,12 @@ class ModerationsController < ApplicationController
       .offset((@page - 1) * ENTRIES_PER_PAGE)
       .order("moderations.created_at desc")
       .limit(ENTRIES_PER_PAGE)
+  end
+
+  private
+
+  def moderation_params
+    @moderation_params ||= params.permit(:moderator, :page,
+      what: %i[stories comments tags users domains origins categories])
   end
 end

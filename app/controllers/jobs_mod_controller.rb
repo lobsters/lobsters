9001adc9df76c93e2b4c4d2e1b1ddcef6cb91181
Rahul Gupta <rahul1990gupta@gmail.com>
2025-01-25 14:45:56 +0530

    adds rubocop disable

--- app/controllers/jobs_mod_controller.rb
+++ app/controllers/jobs_mod_controller.rb
@@ -1,5 +1,7 @@
-class JobsModController < ApplicationController
+# rubocop:disable Rails/ApplicationController
+class JobsModController < ActionController::Base
   include Authenticatable
 
   before_action :require_logged_in_moderator
 end
+# rubocop:enable Rails/ApplicationController

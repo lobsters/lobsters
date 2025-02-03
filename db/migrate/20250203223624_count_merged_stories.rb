8b99ce24e45295b4a603c4c172fa7731f077bdca
Peter Bhat Harkins <peter@push.cx>
2025-02-03 17:18:54 -0600

    typo

--- db/migrate/20250203223624_count_merged_stories.rb
+++ db/migrate/20250203223624_count_merged_stories.rb
@@ -1,7 +1,7 @@
 class CountMergedStories < ActiveRecord::Migration[8.0]
   def up
     add_column :stories, :stories_count, :integer, null: false, default: 0
-    Story.update_all("stories_count = (select count(*) from stories as s_innter where s_inner.merged_story_id = stories.id)")
+    Story.update_all("stories_count = (select count(*) from stories as s_inner where s_inner.merged_story_id = stories.id)")
   end
 
   def down

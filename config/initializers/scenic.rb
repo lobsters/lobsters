593150dd47df66a3e824a7b6b279e2048d4cb1e2
Caius Durling <dev@caius.name>
2025-02-06 10:37:31 +0000

    Proxy Scenic to either MySQL or SQLite adapters
    This allows us to manage views in both databases, albeit with a layer of indirection added into the stack. We want to run the primary database on MySQL but the (solid)cache database on SQLite.
    
    Without doing this we either can't manage the primary or the cache database, especially running `bin/rails db:schema:dump`.

--- config/initializers/scenic.rb
+++ config/initializers/scenic.rb
@@ -1,5 +1,25 @@
 # typed: false
 
+scenic_multidb_adapter = Class.new do
+  def initialize
+    @mysql = Scenic::Adapters::MySQL.new
+    @sqlite = Scenic::Adapters::Sqlite.new
+  end
+
+  def method_missing(...)
+    adapter = case ActiveRecord::Base.connection
+    when ActiveRecord::ConnectionAdapters::TrilogyAdapter
+      @mysql
+    when ActiveRecord::ConnectionAdapters::SQLite3Adapter
+      @sqlite
+    else
+      raise "Unsupported adapter"
+    end
+
+    adapter.__send__(...)
+  end
+end
+
 Scenic.configure do |config|
-  config.database = Scenic::Adapters::MySQL.new
+  config.database = scenic_multidb_adapter.new
 end

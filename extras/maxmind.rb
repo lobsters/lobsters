5c4ff637affe98855dbc401346b770fc4c63c736
Peter Bhat Harkins <peter@push.cx>
2025-03-03 11:14:44 -0600

    yeah that'll sure cause oomkills if every thread does this

--- extras/maxmind.rb
+++ extras/maxmind.rb
@@ -5,7 +5,7 @@ module Maxmind
     return false unless File.exist? DB_PATH
 
     # database from https://db-ip.com/db/download/ip-to-country-lite
-    @db ||= MaxMindDB.new(DB_PATH)
+    @db ||= MaxMindDB.new(DB_PATH, MaxMindDB::LOW_MEMORY_FILE_READER)
     result = @db.lookup(ip)
     return false unless result.found?
     result.country.iso_code == "GB"

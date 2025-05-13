bc703fd124edfaa4f55928eedd4fdb598c32f212
Olivia Reynolds <olivia@naff.dev>
2025-05-13 19:21:42 +0100

    Expand BAD_NETS to cover more special addresses
    Primarily based off of RFC 6890, which obsoleted RFC 5735. Commented to
    give rationale for deviations from that RFC.

--- extras/sponge.rb
+++ extras/sponge.rb
@@ -46,19 +46,25 @@ class Sponge
 
   attr_accessor :debug, :last_res, :timeout, :ssl_verify
 
-  # rfc3330
+  # primarily sourced from rfc6890
   BAD_NETS = [
     "0.0.0.0/8",
     "10.0.0.0/8",
+    "100.64.0.0/10",
     "127.0.0.0/8",
     "169.254.0.0/16",
     "172.16.0.0/12",
+    "192.0.0.0/24",
+    # "192.0.0.0/29", # mentioned in rfc6890 because of different attributes but is a subset of 192.0.0.0/24
     "192.0.2.0/24",
-    "192.88.99.0/24",
+    # "192.88.99.0/24", # mentioned in rfc6890 but marked Global, 6to4 relay anycast
     "192.168.0.0/16",
     "198.18.0.0/15",
-    "224.0.0.0/4",
-    "240.0.0.0/4"
+    "198.51.100.0/24",
+    "203.0.113.0/24",
+    "224.0.0.0/4", # mentioned in rfc5735 only, multicast addresses
+    "240.0.0.0/4",
+    "255.255.255.255/32"
   ].freeze
 
   # old api

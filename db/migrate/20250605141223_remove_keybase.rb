ea35a9ea46f7e7cedc6eae20443addc2f5ca74b0
Peter Bhat Harkins <peter@push.cx>
2025-06-05 09:42:11 -0500

    rm debugging

--- db/migrate/20250605141223_remove_keybase.rb
+++ db/migrate/20250605141223_remove_keybase.rb
@@ -1,7 +1,6 @@
 class RemoveKeybase < ActiveRecord::Migration[8.0]
   def change
     User.where("settings like '%keybase_signatures%'").find_each do |user|
-      puts user.username
       user.settings.delete :keybase_signatures
       user.save!
     end

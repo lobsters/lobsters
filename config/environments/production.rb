670c89008fdf4b69e47b04fbdac8be6218fb1c02
Peter Bhat Harkins <peter@push.cx>
2025-06-09 15:13:54 -0500

    move root-deploy hook to deploy script so it doesn't fire every boot of rails console/ingress

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -129,6 +129,3 @@ end
 %w[render_template render_partial render_collection].each do |event|
   ActiveSupport::Notifications.unsubscribe "#{event}.action_view"
 end
-
-# see hatchbox/root-deploy
-FileUtils.touch "/home/deploy/lobsters/current"

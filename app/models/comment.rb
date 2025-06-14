51ca06edc3b58aa2ebddd349265cf05fce111b2c
Thomas Dziedzic <thomasdziedzic0@gmail.com>
2025-06-14 17:49:17 -0500

    Notify users asynchronously when creating comments and messages.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -34,7 +34,7 @@ class Comment < ApplicationRecord
   after_destroy :unassign_votes
   # Calling :record_initial_upvote from after_commit instead of after_create minimizes how many
   # queries happen inside the transaction, which seems to be the cause of bug #1238.
-  after_commit :deliver_notifications, :log_hat_use, :mark_submitter, :record_initial_upvote, on: :create
+  after_commit :log_hat_use, :mark_submitter, :record_initial_upvote, on: :create
   after_commit :recreate_links
 
   scope :deleted, -> { where(is_deleted: true) }
@@ -305,78 +305,6 @@ class Comment < ApplicationRecord
 
     story.update_cached_columns
     self.user.refresh_counts!
-  end
-
-  def deliver_notifications
-    notified = deliver_reply_notifications
-    deliver_mention_notifications(notified)
-  end
-
-  def deliver_mention_notifications(notified = [])
-    to_notify = plaintext_comment.scan(/\B[@~]([\w\-]+)/).flatten.uniq - notified - [user.username]
-    User.active.where(username: to_notify).find_each do |u|
-      if u.email_mentions?
-        begin
-          EmailReplyMailer.mention(self, u).deliver_now
-        rescue => e
-          # Rails.logger.error "error e-mailing #{u.email}: #{e}"
-        end
-      end
-
-      if u.pushover_mentions?
-        u.pushover!(
-          title: "#{Rails.application.name} mention by " \
-            "#{user.username} on #{story.title}",
-          message: plaintext_comment,
-          url: Routes.comment_target_url(self),
-          url_title: "Reply to #{user.username}"
-        )
-      end
-    end
-  end
-
-  def users_following_thread
-    users_following_thread = Set.new
-    if user.id != story.user.id && story.user_is_following
-      users_following_thread << story.user
-    end
-
-    if parent_comment_id &&
-        (u = parent_comment.try(:user)) &&
-        u.id != user.id &&
-        u.is_active?
-      users_following_thread << u
-    end
-
-    users_following_thread
-  end
-
-  def deliver_reply_notifications
-    notified = []
-
-    users_following_thread.each do |u|
-      if u.email_replies?
-        begin
-          EmailReplyMailer.reply(self, u).deliver_now
-          notified << u.username
-        rescue => e
-          # Rails.logger.error "error e-mailing #{u.email}: #{e}"
-        end
-      end
-
-      if u.pushover_replies?
-        u.pushover!(
-          title: "#{Rails.application.name} reply from " \
-            "#{user.username} on #{story.title}",
-          message: plaintext_comment,
-          url: Routes.comment_target_url(self),
-          url_title: "Reply to #{user.username}"
-        )
-        notified << u.username
-      end
-    end
-
-    notified
   end
 
   def depth_permits_reply?

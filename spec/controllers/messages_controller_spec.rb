d1194e52ac9e08c30ec9edf409df5274a09bf6cf
Thomas Dziedzic <thomasdziedzic0@gmail.com>
2025-06-14 22:03:48 -0500

    fix linter styling

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -15,7 +15,7 @@ describe MessagesController do
   describe "POST create" do
     it "schedules a notification job" do
       stub_login_as sender
-      post :create, params: {message: {recipient_username: recipient.username, subject: 'hello', body: 'secret message'}}
+      post :create, params: {message: {recipient_username: recipient.username, subject: "hello", body: "secret message"}}
       expect(response.status).to eq(302)
       expect(NotifyJob).to have_been_enqueued.exactly(:once)
     end
--- spec/jobs/notify_job_spec.rb
+++ spec/jobs/notify_job_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe NotifyJob, type: :job do
   describe "comment notifications" do

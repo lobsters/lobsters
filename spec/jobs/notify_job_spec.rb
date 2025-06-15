e773258a0f21539b41abf43b38dfe247ea7097a7
Thomas Dziedzic <thomasdziedzic0@gmail.com>
2025-06-14 22:01:35 -0500

    write a test for message notifications

--- spec/jobs/notify_job_spec.rb
+++ spec/jobs/notify_job_spec.rb
@@ -83,4 +83,16 @@ RSpec.describe NotifyJob, type: :job do
       expect(sent_emails[0].subject).to match(/Reply from #{sender.username}/)
     end
   end
+
+  describe "message notifications" do
+    it "sends a message notification" do
+      recipient = create(:user)
+      recipient.settings["email_messages"] = true
+      recipient.save!
+      message = create(:message, recipient: recipient)
+      NotifyJob.perform_now(message)
+      expect(sent_emails.size).to eq(1)
+      expect(sent_emails[0].subject).to match(/Private Message from #{message.author_username}/)
+    end
+  end
 end

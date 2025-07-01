3a054a016aebc4975f85336e9b4058c1d5e5bd5f
Peter Bhat Harkins <peter@push.cx>
2025-07-01 08:38:11 -0500

    ugh still waking up, should use the variable from the controller, not my console testing

--- app/controllers/csp_controller.rb
+++ app/controllers/csp_controller.rb
@@ -5,14 +5,15 @@ class CspController < ApplicationController
   skip_before_action :authenticate_user
 
   def violation_report
-    json = JSON.parse(request.body.read)
+    body = request.body.read
+    json = JSON.parse(body)
 
     return head :bad_request unless
       request.content_type == "application/csp-report" && json&.is_a?(Hash) && json["csp-report"].is_a?(Hash)
 
     report = json["csp-report"]
     Telebugs.context :report, report
-    Telebugs.message sample, fingerprint: ["csp-violation", report.dig("blocked-uri"), report.dig("effective-directive")]
+    Telebugs.message body, fingerprint: ["csp-violation", report.dig("blocked-uri"), report.dig("effective-directive")]
 
     head :ok
   rescue JSON::ParserError

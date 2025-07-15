174ef78338200b15ef9d0df3b3f99b6cfb330d34
Peter Bhat Harkins <peter@push.cx>
2025-07-15 10:55:49 -0500

    session_store: fix login flapping with 'same_site: :lax'

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -7,7 +7,11 @@ Lobsters::Application.config.session_store :cookie_store,
   key: "lobster_trap",
   expire_after: 1.month,
   httponly: true,
-  same_site: :strict
-# :secure commented out because it's redundant with config.force_ssl
-# https://api.rubyonrails.org/v5.2.8.1/classes/ActionDispatch/SSL.html
+  # :same_site isn't :strict because users who navigate from email/mastodon/etc links get that first
+  # page logged out, then their next page is logged in, both of which look like the site security code
+  # has big bugs.
+  # https://developer.mozilla.org/en-US/docs/Web/Security/Practical_implementation_guides/Cookies#samesite
+  same_site: :lax
+# :secure commented out because it's redundant with config.force_ssl; if it's set in the test env
+# the tests fail because they access via http https://api.rubyonrails.org/v5.2.8.1/classes/ActionDispatch/SSL.html
 # secure: true

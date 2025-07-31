e85861ac174ed084123ab241b5904b0b31d04a41
Yuanji <yuanji@yuanji.dev>
2025-07-31 21:15:05 +0900

    fix(mod_note): prevent creating duplicate mod note when preview

--- app/models/mod_note.rb
+++ app/models/mod_note.rb
@@ -209,7 +209,7 @@ class ModNote < ApplicationRecord
   # and save attempts, leading to duplicate notes
   def self.create_without_dupe!(attrs)
     latest = attrs[:user].mod_notes.last
-    return latest if latest && attrs[:moderator] == latest.moderator && attrs[:note] == latest.note
+    return latest if latest && attrs[:moderator] == latest.moderator && attrs[:note].strip == latest.note
     ModNote.create!(attrs)
   end
 end

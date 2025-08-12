1762a317c171d1970499da15ce6274f949f631e1
Jean Lim <sjeanc911@gmail.com>
2025-08-12 23:14:18 +0800

    Remove unsafe string concats in hats helper

--- app/helpers/hats_helper.rb
+++ app/helpers/hats_helper.rb
@@ -1,32 +1,25 @@
 # typed: false
 
 module HatsHelper
-  def hat_html_label(hat)
+  def styled_hat(hat)
     hl = hat.link.present? && hat.link.match(/^https?:\/\//)
 
-    h = "<span class=\"hat " \
-      "hat_#{hat.hat.gsub(/[^A-Za-z0-9]/, "_").downcase}\" " \
-      "title=\"Granted #{hat.created_at.strftime("%Y-%m-%d")}"
-
     if !hl && hat.link.present?
-      h << " - #{ERB::Util.html_escape(hat.sanitized_link)}"
+      sanitized_link = " - #{ERB::Util.html_escape(hat.sanitized_link)}"
     end
 
-    h << "\">" \
-      "<span class=\"crown\">"
-
-    if hl
-      h << "<a href=\"#{ERB::Util.html_escape(hat.link)}\" target=\"_blank\">"
+    content_tag(:span, class: "hat hat_#{hat.hat.gsub(/[^A-Za-z0-9]/, "_").downcase}", title: "Granted #{hat.created_at.strftime("%Y-%m-%d")}#{sanitized_link if sanitized_link}") do
+      concat hat_text(hat, hl)
     end
+  end
 
-    h << ERB::Util.html_escape(hat.hat)
-
-    if hl
-      h << "</a>"
+  def hat_text(hat, hl)
+    content_tag(:span, class: "crown") do
+      if hl
+        concat link_to(ERB::Util.html_escape(hat.hat), ERB::Util.html_escape(hat.link), target: "_blank", rel: "noopener")
+      else
+        concat ERB::Util.html_escape(hat.hat)
+      end
     end
-
-    h << "</span></span>"
-
-    h.html_safe
   end
 end

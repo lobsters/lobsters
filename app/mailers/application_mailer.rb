4375287267cda183d9a94bb302c54818713c32d5
Akarsh-Choudhary <akarsh.choudhary2004@proton.me>
2025-08-15 12:02:15 +0100

    Add check to ApplicationMailer
    Ensure that emails are not sent out to domains known for hosting disposable emails.

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,14 @@
 # typed: false
 
 class ApplicationMailer < ActionMailer::Base
+  include EmailBlocklistHelper
+
   default from: "#{Rails.application.name} <nobody@#{Rails.application.domain}>"
+  after_action :check_blocklist
+
+  def check_blocklist
+    if email_on_blocklist?(mail.to[0])
+      mail.perform_deliveries = false
+    end
+  end
 end

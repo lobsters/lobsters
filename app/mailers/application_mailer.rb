f36ca4621eb939952178635c7097885d08d8672d
Akarsh-Choudhary <akarsh.choudhary2004@proton.me>
2025-08-29 14:15:29 +0100

    fixup! Add check to ApplicationMailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,13 +1,11 @@
 # typed: false
 
 class ApplicationMailer < ActionMailer::Base
-  include EmailBlocklistHelper
-
   default from: "#{Rails.application.name} <nobody@#{Rails.application.domain}>"
-  after_action :check_blocklist
+  after_action :check_email_blocklist
 
-  def check_blocklist
-    if email_on_blocklist?(mail.to[0])
+  def check_email_blocklist
+    if EmailBlocklistValidation.email_on_blocklist?(mail.to[0])
       mail.perform_deliveries = false
     end
   end

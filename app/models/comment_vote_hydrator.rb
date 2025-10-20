56fcea45fb2ba71512d2757aa792e138b27d8300
Sodgerel Mandakhnaran <sm8694@nyu.edu>
2025-10-20 15:48:40 +0900

    Change implementation of CommentVoteHydrator

--- app/models/comment_vote_hydrator.rb
+++ app/models/comment_vote_hydrator.rb
@@ -1,28 +1,27 @@
 class CommentVoteHydrator
   def initialize(comments, user)
     @comments = comments
+    @comment_ids = comments.map(&:id)
     @user = user
+    @hydrated = false
   end
 
-  def get
-    hydrate
+  def [](comment)
+    hydrate unless @hydrated
+    comment.instance_variable_set(:@current_vote, @votes[comment.id])
+    comment.instance_variable_set(:@vote_summary, @vote_summaries[comment.id])
+    comment.instance_variable_set(:@current_reply, @current_user_reply_parents.has_key?(comment.id))
+    comment
   end
 
   private
 
   def hydrate
-    return @comments unless @user && @comments.any?
+    return unless @user && @comment_ids.any?
 
-    comment_ids = @comments.map(&:id)
-    votes = Vote.comment_votes_by_user_for_comment_ids_hash(@user.id, comment_ids)
-    vote_summaries = Vote.comment_vote_summaries(comment_ids)
-    current_user_reply_parents = @user.ids_replied_to(comment_ids)
-
-    @comments.each do |c|
-      c.current_vote = votes[c.id]
-      c.vote_summary = vote_summaries[c.id]
-      c.current_reply = current_user_reply_parents.has_key? c.id
-    end
-    @comments
+    @votes = Vote.comment_votes_by_user_for_comment_ids_hash(@user.id, @comment_ids)
+    @vote_summaries = Vote.comment_vote_summaries(@comment_ids)
+    @current_user_reply_parents = @user.ids_replied_to(@comment_ids)
+    @hydrated = true
   end
 end

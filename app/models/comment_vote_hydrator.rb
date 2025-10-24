c9747e61457e8aa2c418e5b74fac8d180bd8a4a4
Sodgerel Mandakhnaran <sm8694@nyu.edu>
2025-10-24 16:10:16 +0900

    Fix error given by security analyzer

--- app/models/comment_vote_hydrator.rb
+++ app/models/comment_vote_hydrator.rb
@@ -11,7 +11,6 @@ class CommentVoteHydrator
     comment.instance_variable_set(:@current_vote, @votes&.dig(comment.id))
     comment.instance_variable_set(:@vote_summary, @vote_summaries&.dig(comment.id))
     comment.instance_variable_set(:@current_reply, @current_user_reply_parents&.key?(comment.id) || false)
-    comment
   end
 
   private

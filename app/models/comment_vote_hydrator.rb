b9d9fc7473d356142d9376ad521e9fb9e69d5beb
Sodgerel Mandakhnaran <sm8694@nyu.edu>
2025-11-14 15:25:56 +0900

    Move hydration queries into the init method

--- app/models/comment_vote_hydrator.rb
+++ app/models/comment_vote_hydrator.rb
@@ -1,26 +1,22 @@
 class CommentVoteHydrator
   def initialize(comments, user)
     @comments = comments
-    @comment_ids = comments.map(&:id)
     @user = user
-    @hydrated = false
+    if @user.nil? || comments.empty?
+      @votes = {}
+      @vote_summaries = {}
+      @current_user_reply_parents = {}
+    else
+      comment_ids = comments.map(&:id)
+      @votes = Vote.comment_votes_by_user_for_comment_ids_hash(@user.id, comment_ids)
+      @vote_summaries = Vote.comment_vote_summaries(comment_ids)
+      @current_user_reply_parents = @user.ids_replied_to(comment_ids)
+    end
   end
 
   def [](comment)
-    hydrate unless @hydrated
     comment.current_vote = @votes&.dig(comment.id)
     comment.vote_summary = @vote_summaries&.dig(comment.id)
     comment.current_reply = @current_user_reply_parents&.key?(comment.id) || false
-  end
-
-  private
-
-  def hydrate
-    return unless @user && @comment_ids.any?
-
-    @votes = Vote.comment_votes_by_user_for_comment_ids_hash(@user.id, @comment_ids)
-    @vote_summaries = Vote.comment_vote_summaries(@comment_ids)
-    @current_user_reply_parents = @user.ids_replied_to(@comment_ids)
-    @hydrated = true
   end
 end

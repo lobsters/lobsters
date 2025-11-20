c5516faab8d9ad0e6d7c841e49b7fc31053ba0bb
Peter Bhat Harkins <peter@push.cx>
2025-11-20 10:29:56 -0600

    send mail errors to telebugs

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -4,6 +4,12 @@ class ApplicationMailer < ActionMailer::Base
   default from: "#{Rails.application.name} <nobody@#{Rails.application.domain}>"
   after_action :check_email_blocklist
 
+  # https://github.com/rails/solid_queue#error-reporting-on-jobs
+  ActionMailer::MailDeliveryJob.rescue_from(Exception) do |exception|
+    Rails.error.report(exception)
+    raise exception
+  end
+
   def check_email_blocklist
     if EmailBlocklistValidation.email_on_blocklist?(mail.to[0])
       mail.perform_deliveries = false

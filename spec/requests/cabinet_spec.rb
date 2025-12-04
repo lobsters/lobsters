46ddc66f45784818b5c7a06869299e77769e7370
Peter Bhat Harkins <peter@push.cx>
2025-12-03 19:35:49 -0600

    smoke test

--- spec/requests/cabinet_spec.rb
+++ spec/requests/cabinet_spec.rb
@@ -1,7 +1,8 @@
 require "rails_helper"
 
 RSpec.describe "Cabinets", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  it "loads" do
+    get "/cabinet"
+    expect(response.status).to eq(200)
   end
 end

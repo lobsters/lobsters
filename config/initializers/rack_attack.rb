042577da44aa7477d3ec57764e29f6e7d9145477
Peter Bhat Harkins <peter@push.cx>
2025-12-09 13:55:04 -0600

    seems to be a browser scripting tool used for spam, but acts like a metasearch engine

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -44,7 +44,8 @@ Rack::Attack.throttle("SEO/spam tools", limit: 1, period: 1.week.to_i) do |reque
   request.ip if request.user_agent.try(:include?, "www.semrush.com/bot") ||
     request.user_agent.try(:include?, "webmeup-crawler.com") ||
     request.user_agent.try(:include?, "ChatGPT-User") ||
-    request.user_agent.try(:include?, "mentions.us")
+    request.user_agent.try(:include?, "mentions.us") ||
+    request.user_agent.try(:include?, "axios")
 end
 
 Rack::Attack.throttle("a particular bad bot", limit: 1, period: 1.week.to_i) do |request|

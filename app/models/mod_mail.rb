bb205380a5f62d314dd04cf0596a7fc82485d2bd
Rachael Wright-Munn <chaelcodes@gmail.com>
2025-12-11 19:04:59 +0000

    Display and save Comment and Story references

--- app/models/mod_mail.rb
+++ app/models/mod_mail.rb
@@ -1,11 +1,20 @@
 class ModMail < ApplicationRecord
   has_many :mod_mail_references
   has_many :comment_references, through: :mod_mail_references, source: :reference, source_type: 'Comment'
+  has_many :story_references, through: :mod_mail_references, source: :reference, source_type: 'Story'
   has_many :mod_mail_recipients
   has_many :recipients, through: :mod_mail_recipients, source: :user, class_name: 'User'
   has_many :mod_mail_messages
 
   validates :recipients, presence: true
+
+  def comment_reference_short_ids
+    comment_references.pluck(:short_id).join(' ')
+  end
+
+  def story_reference_short_ids
+    story_references.pluck(:short_id).join(' ')
+  end
 
   def recipient_usernames
     recipients.pluck(:username).join(' ')
--- app/models/mod_mail_reference.rb
+++ app/models/mod_mail_reference.rb
@@ -2,5 +2,5 @@ class ModMailReference < ApplicationRecord
   belongs_to :mod_mail
   belongs_to :reference, polymorphic: true
 
-  validates :reference_type, inclusion: {in: %w[Comment]}
+  validates :reference_type, inclusion: {in: %w[Comment Story]}
 end

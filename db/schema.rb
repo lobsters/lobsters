9e3be0abc61d2de9d9d2332b54047e7e1e8656db
Felipe Vogel <fps.vogel@gmail.com>
2025-12-16 17:17:33 -0500

    Fix database_consistency MissingIndexChecker, ForeignKeyChecker
    From the database_consistency gem documentation:
    
    MissingIndexChecker:
    https://github.com/djezzzl/database_consistency/wiki/missingindexchecker
    
    Imagine your model has a `has_one :user` association but has no index in
    the database. In this case, querying the database to get the associated
    instance can be very inefficient. Mostly, you'll need an index to
    process such queries fast.
    
    Additionally, `has_one` associations should be unique on the database
    level to avoid unexpected behavior.
    
    ForeignKeyChecker:
    https://github.com/djezzzl/database_consistency/wiki/foreignkeychecker
    
    Imagine your model has `belongs_to :user`. It can happen that the user,
    it's being belonging to, may not be existing anymore in the database.
    This could bring bugs and in order to ensure data consistency, you need
    to have a foreign key constraints in the database.

--- db/schema.rb
+++ db/schema.rb
@@ -473,9 +473,10 @@ ActiveRecord::Schema[8.0].define(version: 2026_01_06_182240) do
 
   create_table "usernames", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.string "username", null: false
-    t.bigint "user_id", null: false
+    t.bigint "user_id", null: false, unsigned: true
     t.datetime "created_at", null: false
     t.datetime "renamed_away_at"
+    t.index ["user_id"], name: "fk_rails_74bbef8f63"
   end
 
   create_table "users", id: { type: :bigint, unsigned: true }, charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
@@ -589,6 +590,7 @@ ActiveRecord::Schema[8.0].define(version: 2026_01_06_182240) do
   add_foreign_key "taggings", "stories", name: "taggings_story_id_fk"
   add_foreign_key "taggings", "tags", name: "taggings_tag_id_fk", on_update: :cascade, on_delete: :cascade
   add_foreign_key "tags", "categories"
+  add_foreign_key "usernames", "users"
   add_foreign_key "users", "users", column: "banned_by_user_id", name: "users_banned_by_user_id_fk"
   add_foreign_key "users", "users", column: "disabled_invite_by_user_id", name: "users_disabled_invite_by_user_id_fk"
   add_foreign_key "users", "users", column: "invited_by_user_id", name: "users_invited_by_user_id_fk"

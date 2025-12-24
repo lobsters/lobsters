fc686932b22b8004a3eda45c455a100b733b07ff
Peter Bhat Harkins <peter@push.cx>
2025-12-24 17:05:45 -0600

    better user-agent for our fetches

--- extras/sponge.rb
+++ extras/sponge.rb
@@ -202,7 +202,7 @@ class Sponge
       "Host" => uri.host,
       "Cookie" => cookies(uri.host),
       "Referer" => url.to_s,
-      "User-Agent" => "Mozilla/5.0 (compatible)"
+      "User-Agent" => "Mozilla/5.0 (compatible) #{Rails.application.domain}"
     }.merge(send_headers || {})
 
     if uri.user

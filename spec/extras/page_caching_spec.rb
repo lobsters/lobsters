9cae6c5e364fb737cc6543a070831cd6a4b758f1
Caius Durling <dev@caius.name>
2025-12-28 16:24:24 +0000

    Patch PageCache to handle generated cache files too long for filesystem
    I've assumed the filesystem can handle <= 255 character filenames

--- spec/extras/page_caching_spec.rb
+++ spec/extras/page_caching_spec.rb
@@ -18,6 +18,20 @@ RSpec.describe "page caching monkeypatch" do
         expect(page_cache.cache_file("youtube.com.html", "html")).to eq("youtube.com.html")
       end
     end
+
+    context "with a generated filename fewer than 256 characters" do
+      it "returns the generated file path" do
+        expect(page_cache.cache_file("something-goes-here", "html")).to eq("something-goes-here.html")
+      end
+    end
+
+    context "with a generated filename equal or greater than 256 characters" do
+      it "returns the generated file path" do
+        aaaaa_path = "a" * 255
+        expect(page_cache.cache_file(aaaaa_path, "html")).to eq(
+          Digest::SHA256.hexdigest(aaaaa_path + ".html") + ".html"
+        )
+      end
     end
   end
 end

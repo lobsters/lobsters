e6ca7f452fd05b4d4bc1f26da793938f57a7dc1d
Caius Durling <dev@caius.name>
2026-01-19 20:59:47 +0000

    Improve page cache filename handling (#1842)
    * Fix bug in PageCache#cache_file monkeypatch appending html extension
    
    Also add some tests so we know if this behaviour changes
    
    * Patch PageCache to handle generated cache files too long for filesystem
    
    I've assumed the filesystem can handle <= 255 character filenames
    
    * Ignore standardrb violation with standardrb comment
    
    * Intercept file too long error when caching pages

--- config/initializers/page_caching_monkeypatch.rb
+++ config/initializers/page_caching_monkeypatch.rb
@@ -12,14 +12,28 @@
 # report or contribute a fix upstream.
 #
 # We only cache HTML pages, so this monkeypatch forces the '.html' extension.
+#
+# We also catch an error when caching files if the computed filename on disk is too long, due to the
+# filesystem used in production only supporting 254 character names.
 
 require "actionpack/page_caching"
 
-# rubocop:disable Lint/ConstantDefinitionInBlock
-
 ActiveSupport.on_load(:action_controller) do
-  module ActionController::Caching::Pages
+  module ActionController::Caching::Pages # standard:disable Lint/ConstantDefinitionInBlock
     class PageCache
+      module WritePatch
+        # Override: refuse to cache paths with long filenames
+        def write(...)
+          super
+        rescue Errno::ENAMETOOLONG => e
+          # #1826 - Handle write errors from filenames being longer than 255 bytes
+          Rails.logger.info "Failed to cache page #{e.inspect}"
+          nil
+        end
+      end
+
+      prepend WritePatch
+
       def cache_file(path, extension)
         name = if path.empty? || path =~ %r{\A/+\z}
           "/index"
@@ -31,7 +45,7 @@ ActiveSupport.on_load(:action_controller) do
         #   if File.extname(name).empty?
         # monkeypatch:
         if File.extname(name) != ".html"
-          name + (extension || default_extension)
+          name + "." + (extension || default_extension)
         else
           name
         end
@@ -39,4 +53,3 @@ ActiveSupport.on_load(:action_controller) do
     end
   end
 end
-# rubocop:enable Lint/ConstantDefinitionInBlock

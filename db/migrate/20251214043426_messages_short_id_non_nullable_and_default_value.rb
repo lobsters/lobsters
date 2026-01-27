8bac6b0c59b79c3e9aef584d807e7213e7a5d884
Felipe Vogel <fps.vogel@gmail.com>
2026-01-27 18:02:48 -0500

    Abort migration if Messages with a nil short_id exist

--- db/migrate/20251214043426_messages_short_id_non_nullable_and_default_value.rb
+++ db/migrate/20251214043426_messages_short_id_non_nullable_and_default_value.rb
@@ -1,6 +1,9 @@
 class MessagesShortIdNonNullableAndDefaultValue < ActiveRecord::Migration[8.0]
   def change
-    Message.where(short_id: nil).update_all(short_id: "")
+    if Message.where(short_id: nil).exists?
+      raise "One or more Messages with a nil short_id exist."
+    end
+
     change_column_null :messages, :short_id, false
     change_column_default :messages, :short_id, from: nil, to: ""
   end

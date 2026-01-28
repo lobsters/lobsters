51d5d1c4f2f2a57b07d54541bcc29efdf561531b
Thomas Dziedzic <thomasdziedzic0@gmail.com>
2026-01-28 12:34:56 -0600

    copy comment about not editing migrations

--- db/migrate/20250818182728_add_fts_tables.rb
+++ db/migrate/20250818182728_add_fts_tables.rb
@@ -1,3 +1,9 @@
+# If you are reading this file because Rails complains that there isn't a
+# version on this migration, stop.
+#
+# Create your database with `rails db:schema:load`, not by running all these.
+# We have migrations to migrate live databases, not create them, and we do not
+# want a PR to 'fix' migrations.
 class AddFtsTables < ActiveRecord::Migration[8.0]
   def change
     # create preferred contentless-delete tables as described in:

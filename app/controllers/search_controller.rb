99034f9514fd4e3e9ce4b679dfebd8dcee3be753
Peter Bhat Harkins <peter@push.cx>
2026-02-03 11:25:28 -0600

    searchbot slamming the site #1814

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -8,7 +8,13 @@ class SearchController < ApplicationController
     @title = "Search"
 
     @search = Search.new(search_params, @user)
-    @results = @search.results
+
+    if !@user && params[:q].to_s.starts_with?("https://")
+      flash[:error] = "Sorry, you have to log in to search for a URL. We're getting hammered by a spambot with many thousands of IPs. More info at https://github.com/lobsters/lobsters/issues/1814"
+      @results = []
+    else
+      @results = @search.results
+    end
 
     if @user && @search.results
       if params[:what] == "stories"

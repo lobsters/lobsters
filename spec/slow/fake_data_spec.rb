bed053f7caa0a1a7a78883b5b3dfaa24132a32cb
Thomas Dziedzic <thomasdziedzic0@gmail.com>
2026-02-23 17:20:16 -0600

    Fix fake_data test
    Before:
    
    ```
    tom@debian:~/work/lobsters-original$ rails db:drop db:create db:schema:load && bundle exec rspec ./spec/slow/fake_data_spec.rb
    
    ...
    
    Failures:
    
      1) fake_data runs
         Failure/Error: before { Rails.application.load_seed }
    
         ActiveRecord::RecordInvalid:
           Validation failed: Username has already been taken, Email has already been taken
         # ./db/seeds.rb:2:in '<top (required)>'
         # ./spec/slow/fake_data_spec.rb:8:in 'block (2 levels) in <top (required)>'
    
    Finished in 2.01 seconds (files took 2.77 seconds to load)
    1 example, 1 failure
    
    ...
    ```
    
    After, the tests pass.

--- spec/slow/fake_data_spec.rb
+++ spec/slow/fake_data_spec.rb
@@ -5,7 +5,8 @@ require "rails_helper"
 Rails.application.load_tasks
 
 describe "fake_data" do
-  before { Rails.application.load_seed }
+  # Delete the system user created by the rails helper since the seeds contain the same user.
+  before { User.find_by(username: "System").destroy && Rails.application.load_seed }
 
   # basic smoke test, task shouldn't throw exceptions
   it "runs" do

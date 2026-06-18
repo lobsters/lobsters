lobsters/lobsters

commit 6d2df326104b7c4517dbc9b872072d4a084bc107

Fix flash messages not shown to non logged in users
Fixes #2042

app/controllers/application_controller.rb
@@ -92,7 +92,7 @@ class ApplicationController < ActionController::Base
   def clear_session_cookie?
     # If the session has been loaded, it will contain some default keys and not
     # be `empty?`
-    !@user && session.keys.all? { |k| SESSION_DEFAULT_KEYS.include?(k.to_s) }
+    !@user && session.keys.all? { |k| SESSION_DEFAULT_KEYS.include?(k.to_s) } && flash.empty?
   end
 
   def find_user_from_rss_token

spec/features/filter_spec.rb
@@ -24,6 +24,8 @@ RSpec.feature "Filtering" do
         expect(page).to have_checked_field(tag.description)
         expect(page.driver.request.cookies["tag_filters"]).to eq(tag.to_param)
 
+        expect(page).to have_content("Your filters have been updated.")
+
         visit "/"
         expect(page).to_not have_content(story.title)
       end

commit dd0f6c14f67e1a4c82a25429382cdb9932403d21

don't cache page if it has flash messages, caused by 6d2df326, fix #2080

app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class ApplicationController < ActionController::Base
   SESSION_DEFAULT_KEYS = %w[session_id _csrf_token]
   # match this in caddy config for bypassing the file cache
   TAG_FILTER_COOKIE = :tag_filters
-  CACHE_PAGE = proc { @user.blank? && cookies[TAG_FILTER_COOKIE].blank? }
+  CACHE_PAGE = proc { @user.blank? && cookies[TAG_FILTER_COOKIE].blank? && clear_session_cookie? }
 
   # copied from https://github.com/rack/rack/blob/main/lib/rack/utils.rb
   VALID_COOKIE_KEY = /\A[!#$%&'*+\-.\^_`|~0-9a-zA-Z]+\z/
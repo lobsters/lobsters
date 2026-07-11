08a3f84466124bcd72b9d51a3517635493cfd1e1
Thomas Dziedzic <thomasdziedzic0@gmail.com>
2026-07-11 09:04:26 -0500

    add custom log location

--- config/initializers/slow_query_log.rb
+++ config/initializers/slow_query_log.rb
@@ -1,9 +1,10 @@
 if ENV["ENABLE_SLOW_QUERY_LOGS"] == "true"
   threshold = ENV["SLOW_QUERY_THRESHOLD_MS"].present? ? ENV["SLOW_QUERY_THRESHOLD_MS"].to_f : 100
+  logger = ActiveSupport::Logger.new(Rails.root.join("log/slow_query.log"))
 
   ActiveSupport::Notifications.subscribe("sql.active_record") do |event|
     if event.duration > threshold
-      Rails.logger.warn("[SLOW QUERY] #{event.duration.round(1)} ms, #{event.payload[:sql]}")
+      logger.warn("[SLOW QUERY] #{event.duration.round(1)} ms, #{event.payload[:sql]}")
     end
   end
 end

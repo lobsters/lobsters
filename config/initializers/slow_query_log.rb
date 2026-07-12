dffb5dcb13ce1975014f486642ae663a47772535
Thomas Dziedzic <thomasdziedzic0@gmail.com>
2026-07-12 07:59:09 -0500

    add timestamp to slow query log

--- config/initializers/slow_query_log.rb
+++ config/initializers/slow_query_log.rb
@@ -1,6 +1,7 @@
 if ENV["ENABLE_SLOW_QUERY_LOGS"] == "true"
   threshold = ENV["SLOW_QUERY_THRESHOLD_MS"].present? ? ENV["SLOW_QUERY_THRESHOLD_MS"].to_f : 100
   logger = ActiveSupport::Logger.new(Rails.root.join("log/slow_query.log"))
+  logger.formatter = Logger::Formatter.new
 
   ActiveSupport::Notifications.subscribe("sql.active_record") do |event|
     if event.duration > threshold

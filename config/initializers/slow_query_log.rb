ea167b5e835e625a97c8b5cd252d497a370bf969
Zurtar <ethan.cook3552@gmail.com>
2026-07-14 13:09:52 +0000

    switch slow query logging to json

--- config/initializers/slow_query_log.rb
+++ config/initializers/slow_query_log.rb
@@ -1,11 +1,16 @@
 if ENV["ENABLE_SLOW_QUERY_LOGS"] == "true"
   threshold = ENV["SLOW_QUERY_THRESHOLD_MS"].present? ? ENV["SLOW_QUERY_THRESHOLD_MS"].to_f : 100
   logger = ActiveSupport::Logger.new(Rails.root.join("log/slow_query.log"))
-  logger.formatter = Logger::Formatter.new
+  logger.formatter = proc do |severity, time, _progname, msg|
+    {
+      timestamp: time.utc.iso8601(3),
+      severity: severity
+    }.merge(msg).to_json + "\n"
+  end
 
   ActiveSupport::Notifications.subscribe("sql.active_record") do |event|
     if event.duration > threshold
-      logger.warn("[SLOW QUERY] #{event.duration.round(1)} ms, #{event.payload[:sql]}")
+      logger.warn(duration_ms: event.duration.round(1), sql: event.payload[:sql])
     end
   end
 end

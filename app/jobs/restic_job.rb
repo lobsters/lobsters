7d6ca455dc08aaee238620e63ee74c5fba32eb67
Peter Bhat Harkins <peter@push.cx>
2026-07-27 10:06:15 -0500

    back up bash, sqlite, and irb histories (the secret source of my powers), tx linkdd

--- app/jobs/restic_job.rb
+++ app/jobs/restic_job.rb
@@ -2,13 +2,14 @@ class ResticJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    shared = "/home/deploy/lobsters/shared"
+    home = "/home/deploy"
+    shared = "#{home}/lobsters/shared"
     unless File.directory?(shared)
       raise "ResticJob: shared path '#{shared}' does not exist, can't back up"
     end
     db_path = Rails.root.join("storage/primary.sqlite3")
     system("sqlite3 #{db_path} \".backup '#{shared}/database-backups/primary.sqlite3'\"", exception: true)
     # must use . instead of source because prod is using sh instead of bash
-    system(". #{shared}/etc/restic-env ; restic backup --no-scan #{shared}/etc #{shared}/log #{shared}/database-backups", exception: true)
+    system(". #{shared}/etc/restic-env ; restic backup --no-scan #{shared}/etc #{shared}/log #{shared}/database-backups #{home}/.*_history", exception: true)
   end
 end

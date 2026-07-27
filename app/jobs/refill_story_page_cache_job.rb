465c3dd3c30ee2a8dab2074ed6830abebc5778dc
Peter Bhat Harkins <peter@push.cx>
2026-07-26 20:21:37 -0500

    work in smaller batches to avoid holding write lock too long

--- app/jobs/refill_story_page_cache_job.rb
+++ app/jobs/refill_story_page_cache_job.rb
@@ -3,7 +3,7 @@ class RefillStoryPageCacheJob < ApplicationJob
   limits_concurrency to: 1, key: "prefill_page_cache"
 
   def perform
-    Story.unmerged.not_deleted(nil).select(:id, :short_id, :title).find_in_batches(order: :desc) do |batch|
+    Story.unmerged.not_deleted(nil).select(:id, :short_id, :title).find_in_batches(batch_size: 100, order: :desc) do |batch|
       jobs = batch.map { |story| CachePageJob.new(Routes.title_path(story)).set(queue: :idle) }
       ActiveJob.perform_all_later(jobs)
     end

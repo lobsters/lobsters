05ba4201321e04e4964335fc05d3e9c1e6786e94
oyamairishitomi <h.tezuka0707@gmail.com>
2026-08-24 23:07:19 +0900

    Fix upvote arrow not showing as orange in /inbox/all (fixes #2128)

--- app/controllers/inbox_controller.rb
+++ app/controllers/inbox_controller.rb
@@ -63,6 +63,6 @@ class InboxController < ApplicationController
 
   def apply_current_vote
     comment_notifications = @notifications.filter { |n| n.notifiable_type == "Comment" }.map(&:notifiable)
-    comment_notifications = CommentVoteHydrator.new(comment_notifications, @user)
+    comment_notifications = CommentVoteHydrator.new(comment_notifications, @user).to_a
   end
 end

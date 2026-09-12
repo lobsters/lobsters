8fc779353aad6aacedaf97f53e1bb37c8ffb57b4
Peter Bhat Harkins <peter@push.cx>
2026-09-12 15:11:21 -0500

    mutt merges emails by subject line

--- app/mailers/ban_notification_mailer.rb
+++ app/mailers/ban_notification_mailer.rb
@@ -9,7 +9,7 @@ class BanNotificationMailer < ApplicationMailer
       from: "#{@banner.username} <reply-to@#{Rails.application.domain}>",
       reply_to: "#{@banner.username} <#{@banner.email}>",
       to: user.email,
-      subject: "[#{Rails.application.name}] You have been banned"
+      subject: "[#{Rails.application.name}] account #{user.username} banned"
     )
   end
 end
